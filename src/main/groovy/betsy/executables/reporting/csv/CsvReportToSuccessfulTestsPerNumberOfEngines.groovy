69ce6845c92f53d6b2db1f1d72b2ed177c8e91d2
Simon Harrer <simon.harrer@gmail.com>
2012-10-23 12:30:17 +0200

    More sophisticated statistics.

--- src/main/groovy/betsy/executables/reporting/csv/CsvReportToSuccessfulTestsPerNumberOfEngines.groovy
+++ src/main/groovy/betsy/executables/reporting/csv/CsvReportToSuccessfulTestsPerNumberOfEngines.groovy
@@ -11,7 +11,11 @@ class CsvReportToSuccessfulTestsPerNumberOfEngines {
     CsvReport report
 
     void toCsvReport(PrintStream writer) {
+        int total = report.tests.size()
+
+
         List<ResultPair> results = []
+        writer.println "Raw"
         report.tests.each { test ->
             int successful = test.engineToResult.values().count { it.partial == Result.Support.TOTAL}
             results << new ResultPair(name: test.name, successful: successful)
@@ -22,6 +26,14 @@ class CsvReportToSuccessfulTestsPerNumberOfEngines {
         results.each { result ->
             writer.println "${result.name}\t${result.successful}"
         }
+
+        writer.println "Aggregated"
+        results.groupBy { it.successful }.sort() {it.key}.each { key, values ->
+            writer.println key + "\t" + values.size() + "\t" + (int)((double) values.size() / total * 100) + "%"
+        }
+
+        writer.println "At least one engine"
+        writer.println results.findAll(){it.successful > 0} + "\t" +  (int)((double) values.size() / total * 100) + "%"
 
     }
 
--- src/main/groovy/betsy/executables/reporting/csv/CsvReportToTestsPerEngineAndGroup.groovy
+++ src/main/groovy/betsy/executables/reporting/csv/CsvReportToTestsPerEngineAndGroup.groovy
@@ -6,24 +6,37 @@ class CsvReportToTestsPerEngineAndGroup {
     CsvReport report
 
     void toCsvReport(PrintStream writer) {
-            report.getEngines().each { engine ->
-                int totalSuccessful = 0
-                int totalPartial = 0
-                int totalFailed = 0
-                report.getGroups().each { group ->
-                    Collection<Result> results = group.getResultsPerEngine(engine)
-                    int successful = results.count { it.partial == Result.Support.TOTAL}
-                    totalSuccessful += successful
-                    int partial = results.count { it.partial == Result.Support.PARTIAL}
-                    totalPartial += partial
-                    int failed = results.count { it.partial == Result.Support.NONE}
-                    totalFailed += failed
+        report.getEngines().each { engine ->
+            int totalSuccessful = 0
+            int totalPartial = 0
+            int totalFailed = 0
+            report.getGroups().each { group ->
+                Collection<Result> results = group.getResultsPerEngine(engine)
+                int successful = results.count { it.partial == Result.Support.TOTAL}
+                totalSuccessful += successful
+                int partial = results.count { it.partial == Result.Support.PARTIAL}
+                totalPartial += partial
+                int failed = results.count { it.partial == Result.Support.NONE}
+                totalFailed += failed
 
-                    writer.println "$successful\t${partial == 0 ? '' : partial}\t$failed"
-                }
-
-                writer.println "$totalSuccessful\t${totalPartial == 0 ? '' : totalPartial}\t$totalFailed"
+                writer.println "$successful\t${partial == 0 ? '' : partial}\t$failed"
             }
+
+            writer.println "$totalSuccessful\t${totalPartial == 0 ? '' : totalPartial}\t$totalFailed"
+        }
+
+        writer.println "Engine in %"
+
+        int total = report.tests.size()
+        report.getEngines().each { engine ->
+            int totalSuccessful = 0
+            report.getGroups().each { group ->
+                Collection<Result> results = group.getResultsPerEngine(engine)
+                totalSuccessful += results.count { it.partial == Result.Support.TOTAL}
+            }
+
+            writer.println "${engine.name}\t$totalSuccessful\t${(int)((double) totalSuccessful / total * 100)}%"
+        }
     }
 
     public static void main(String[] args) {
