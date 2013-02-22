c0091d904c1740018f75949937418273ee939502
Simon Harrer <simon.harrer@gmail.com>
2013-02-22 14:07:44 +0100

    Bugfix

--- src/main/tests/configuration/processes/BasicActivityProcesses.groovy
+++ src/main/tests/configuration/processes/BasicActivityProcesses.groovy
@@ -466,7 +466,7 @@ class BasicActivityProcesses {
     )
 
     public final Process ASSIGN_MISMATCHED_ASSIGNMENT_FAILURE = builder.buildBasicActivityProcess(
-            "basic-activities/Assign-MismatchedAssignmentFailure", "An assignment between two incompatible types. A mismatchedAssignmentFailure should be thrown.",
+            "Assign-MismatchedAssignmentFailure", "An assignment between two incompatible types. A mismatchedAssignmentFailure should be thrown.",
             [
                     new TestCase(testSteps: [new TestStep(input: "1", operation: WsdlOperation.SYNC, assertions: [new SoapFaultTestAssertion(faultString: "mismatchedAssignment")])]),
             ]
