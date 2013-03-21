4ceee690bedbc827e6dacf0591b674a77ebd507b
Simon Harrer <simon.harrer@gmail.com>
2013-03-21 12:09:43 +0100

    Adds logging statements for partner service

--- src/main/groovy/betsy/executables/ws/TestPartnerServiceMock.groovy
+++ src/main/groovy/betsy/executables/ws/TestPartnerServiceMock.groovy
@@ -7,7 +7,7 @@ import javax.xml.soap.SOAPFactory
 import javax.xml.soap.SOAPFault
 import javax.xml.namespace.QName
 import javax.xml.ws.soap.SOAPFaultException
-import de.uniba.wiai.dsg.betsy.activities.wsdl.testpartner.ObjectFactory
+
 import de.uniba.wiai.dsg.betsy.activities.wsdl.testpartner.FaultMessage
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -20,6 +20,8 @@ endpointInterface = "de.uniba.wiai.dsg.betsy.activities.wsdl.testpartner.TestPar
 wsdlLocation = "TestPartner.wsdl")
 class TestPartnerServiceMock implements TestPartnerPortType {
 
+    public static final int CONCURRENCY_TIMEOUT = 1000
+
     private final boolean replyInput
 
     private final AtomicInteger concurrentAccesses = new AtomicInteger(0)
@@ -27,11 +29,6 @@ class TestPartnerServiceMock implements TestPartnerPortType {
     private final AtomicInteger totalConcurrentAccesses = new AtomicInteger(0)
 
     private final AtomicInteger totalAccesses = new AtomicInteger(0)
-
-    public TestPartnerServiceMock() {
-        this.replyInput = true
-    }
-
 
     public TestPartnerServiceMock(boolean replyInput) {
         this.replyInput = replyInput
@@ -42,16 +39,19 @@ class TestPartnerServiceMock implements TestPartnerPortType {
     }
 
     public int startProcessSync(int inputPart) {
-        println "Partner: startProcessSync with ${inputPart}"
+        println "[${new Date()}] Partner: startProcessSync with ${inputPart}"
         totalAccesses.incrementAndGet()
 
         if (inputPart == -5) {
+            println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Throwing CustomFault"
+
             SOAPFactory fac = SOAPFactory.newInstance();
             SOAPFault sf = fac.createFault("expected Error", new QName("http://dsg.wiai.uniba.de/betsy/activities/wsdl/testpartner", "CustomFault"))
             throw new SOAPFaultException(sf)
         }
 
         if (inputPart == -6) {
+            println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Throwing Fault"
             FaultMessage fault = new FaultMessage("expected Error", inputPart)
             throw fault
         }
@@ -67,16 +67,20 @@ class TestPartnerServiceMock implements TestPartnerPortType {
         if (inputPart == 100) {
             //magic number for tracking concurrent accesses
             concurrentAccesses.incrementAndGet()
-            Thread.sleep(200)
+            Thread.sleep(CONCURRENCY_TIMEOUT)
 
             int result = 100
             if (concurrentAccesses.get() == 1) {
                 // no concurrency detected
                 result = 0
             } else {
+                println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Concurrency Detected"
                 totalConcurrentAccesses.incrementAndGet()
             }
             concurrentAccesses.decrementAndGet()
+
+            println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Returning ${result}"
+
             return result
         } else if (inputPart == 101) {
             // magic number for querying number of concurrent accesses, do not count to totalAccesses
@@ -84,12 +88,18 @@ class TestPartnerServiceMock implements TestPartnerPortType {
             //reset totalConcurrentAccesses after each query
             int result = totalConcurrentAccesses.get()
             totalConcurrentAccesses.set(0)
+
+            println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Returning ${result}"
+
             return result
         } else if (inputPart == 102) {
             // magic number for querying number of total accesses, do not count to totalAccesses
             int result = totalAccesses.decrementAndGet()
             //reset totalAccesses after each query
             totalAccesses.set(0)
+
+            println "[${new Date()}] Partner: startProcessSync with ${inputPart} - Returning ${result}"
+
             return result
         } else {
             return inputPart
@@ -97,8 +107,7 @@ class TestPartnerServiceMock implements TestPartnerPortType {
     }
 
     public void startProcessWithEmptyMessage() {
-        println "Partner: startProcessWithEmptyMessage"
+        println "[${new Date()}] Partner: startProcessWithEmptyMessage"
     }
-
 
 }
