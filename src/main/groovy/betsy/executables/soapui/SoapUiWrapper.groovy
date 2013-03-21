975bdeb53360c12fd1b3fea0f882821b03e47f45
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2013-03-21 15:36:13 +0100

    Remove unused imports in SoapUiWrapper

--- src/main/groovy/betsy/executables/soapui/SoapUiWrapper.groovy
+++ src/main/groovy/betsy/executables/soapui/SoapUiWrapper.groovy
@@ -1,29 +1,11 @@
 package betsy.executables.soapui
 
 import betsy.data.Process
-import betsy.data.TestCase
-import betsy.data.TestStep
-import betsy.data.WsdlOperation
-import betsy.data.assertions.ExitAssertion
-import betsy.data.assertions.SoapFaultTestAssertion
-import betsy.data.assertions.XpathTestAssertion
-import com.eviware.soapui.config.TestStepConfig
 import com.eviware.soapui.impl.WsdlInterfaceFactory
-import com.eviware.soapui.impl.wsdl.WsdlInterface
 import com.eviware.soapui.impl.wsdl.WsdlProject
 import com.eviware.soapui.impl.wsdl.WsdlProjectFactory
 import com.eviware.soapui.impl.wsdl.WsdlTestSuite
-import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.basic.GroovyScriptAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.basic.SimpleContainsAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.basic.XPathContainsAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.soap.NotSoapFaultAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.soap.SoapFaultAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.assertions.soap.SoapResponseAssertion
-import com.eviware.soapui.impl.wsdl.teststeps.registry.DelayStepFactory
-import com.eviware.soapui.impl.wsdl.teststeps.registry.GroovyScriptStepFactory
-import com.eviware.soapui.impl.wsdl.teststeps.registry.WsdlTestRequestStepFactory
-import com.eviware.soapui.impl.wsdl.teststeps.*
+
 
 /**
  * Creates a soapUI project from a Process.
