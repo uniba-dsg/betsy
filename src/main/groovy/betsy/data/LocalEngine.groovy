0b342ac0c078de129b11144b25962cd51a8095a7
Cedric Röck <git@roecky.net>
2013-03-31 13:50:06 +0200

    adding VirtualizedEngineAPI and LocalEngineAPI

---
+++ src/main/groovy/betsy/data/LocalEngine.groovy
@@ -0,0 +1,34 @@
+package betsy.data
+
+import gnu.cajo.utils.extra.Implements;
+import betsy.data.engines.*
+
+abstract class LocalEngine extends Engine implements LocalEngineAPI {
+
+	/**
+	 * The path <code>server/$engine</code>
+	 *
+	 * @return the path <code>server/$engine</code>
+	 */
+	@Override
+	public String getServerPath() {
+		"server/${getName()}"
+	}
+
+	@Override
+	public boolean equals(o) {
+		if (this.is(o)) return true
+		if (getClass() != o.class) return false
+
+		LocalEngine engine = (LocalEngine) o
+
+		if (getName() != engine.getName()) return false
+
+		return true
+	}
+
+	@Override
+	public int hashCode() {
+		return (getName() != null ? getName().hashCode() : 0)
+	}
+}
