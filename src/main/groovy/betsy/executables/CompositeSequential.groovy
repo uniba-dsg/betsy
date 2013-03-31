c3bd58cc167cf241f8aa6e5f243e1728e8464501
Cedric Röck <git@roecky.net>
2013-03-31 13:58:49 +0200

    Add ExceptionHandling in CompositeSequential, allowing to repeat a test in case of an error caused by VirtualBox

--- src/main/groovy/betsy/executables/CompositeSequential.groovy
+++ src/main/groovy/betsy/executables/CompositeSequential.groovy
@@ -1,73 +1,103 @@
 package betsy.executables
 
+import de.uniba.wiai.dsg.betsy.virtual.host.exceptions.TestFailedException;
 import betsy.data.Engine
+import betsy.data.Process;
 import betsy.executables.generator.TestBuilder
 import betsy.executables.soapui.runner.SoapUiRunner
 
 class CompositeSequential extends Composite {
 
-    protected void executeEngine(Engine engine) {
-        log "${context.testSuite.path}/${engine.name}", {
-            engine.processes.each { process ->
+	protected void executeEngine(Engine engine) {
+		log "${context.testSuite.path}/${engine.name}", {
+			engine.processes.each { process ->
 
-                println "Process ${engine.processes.indexOf(process) + 1} of ${engine.processes.size()}"
+				boolean testProcess = true
+				int testCount = 0;
+				while(testProcess) {
+					testProcess = false
+					testCount++
 
-                log "${process.targetPath}/all", {
-                    try {
-                        // build
-                        log "${process.targetPath}/build", {
+					try {
+						String repeat = testCount > 1 ? "Repeating process" : "Process"
+						println "${repeat} ${engine.processes.indexOf(process) + 1} of ${engine.processes.size()}"
+
+						log "${process.targetPath}/all", {
+							try {
+								// build
+								log "${process.targetPath}/build", {
 
                             log "${process.targetPath}/build_package", {
                                 engine.buildArchives(process)
                             }
 
-                            log "${process.targetPath}/build_test", {
-                                soapui "${process.targetPath}/soapui_generation", {
-                                    new TestBuilder(process: process,
-                                            requestTimeout: context.requestTimeout,
-                                            ant: ant).buildTest()
-                                }
-                            }
-                        }
+									log "${process.targetPath}/build_test", {
+										soapui "${process.targetPath}/soapui_generation", {
+											new TestBuilder(process: process,
+													requestTimeout: context.requestTimeout,
+													ant: ant).buildTest()
+										}
+									}
+								}
 
-                        // setup infrastructure
-                        log "${process.targetPath}/install", {
-                            engine.install()
-                        }
+								// setup infrastructure
+		                        log "${process.targetPath}/install", {
+		                            engine.install()
+		                        }
+		
+		                        log "${process.targetPath}/startup", {
+		                            engine.startup()
+		                        }
 
-                        log "${process.targetPath}/startup", {
-                            engine.startup()
-                        }
+								log "${process.targetPath}/deploy", {
+									ant.echo message: "Deploying process ${process} to engine ${this}"
+									engine.deploy(process)
+									engine.onPostDeployment(process)
+								}
 
-                        log "${process.targetPath}/deploy", {
-                            ant.echo message: "Deploying process ${process} to engine ${this}"
-                            engine.deploy(process)
-                            engine.onPostDeployment(process)
-                        }
+								context.testPartner.publish()
+								log "${process.targetPath}/test", {
+									soapui "${process.targetPath}/soapui_test", {
+										new SoapUiRunner(soapUiProjectFile: process.targetSoapUIFilePath,
+												reportingDirectory: process.targetReportsPath, ant: ant).run()
+									}
+									ant.sleep(milliseconds: 500)
+									engine.storeLogs(process)
+								}
+								context.testPartner.unpublish()
 
-                        context.testPartner.publish()
-                        log "${process.targetPath}/test", {
-                            soapui "${process.targetPath}/soapui_test", {
-                                new SoapUiRunner(soapUiProjectFile: process.targetSoapUIFilePath,
-                                        reportingDirectory: process.targetReportsPath, ant: ant).run()
-                            }
-                            ant.sleep(milliseconds: 500)
-                            engine.storeLogs(process)
-                        }
-                        context.testPartner.unpublish()
+							} finally {
+								// ensure shutdown
+		                        log "${engine.path}/shutdown", {
+		                            engine.shutdown()
+		                        }
+							}
+						}
+					}catch(TestFailedException exception) {
+						println("Process ${engine.processes.indexOf(process) + 1} test failed")
+						if(exception.isTestRepeatable() && testCount <= 2) {
+							testProcess = true
+							// delete old log output by moving to failed tests
 
-                    } finally {
-                        // ensure shutdown
-                        log "${engine.path}/shutdown", {
-                            engine.shutdown()
-                        }
-                    }
-                }
+							def repeatedDir = "${process.engine.path}/failed_repeated_tests"
+							def destDir = "${repeatedDir}/${testCount}_${process.normalizedId}"
+							ant.mkdir(dir: repeatedDir)
+							ant.move(file: process.targetPath, tofile: destDir, force: true, performGCOnFailedDelete: true)
 
+						}else if(exception.isTestRepeatable() && testCount > 2) {
+							throw new IllegalStateException("Process " +
+							"${engine.processes.indexOf(process) + 1} test " +
+							"failed repeatedly: ", exception);
+						}else {
+							throw new IllegalStateException("Process " +
+							"${engine.processes.indexOf(process) + 1} test " +
+							"failed with a severe exception: ", exception);
+						}
+					}
+				} // END WHILE
+			}
 
-            }
-
-        }
-    }
+		}
+	}
 
 }
