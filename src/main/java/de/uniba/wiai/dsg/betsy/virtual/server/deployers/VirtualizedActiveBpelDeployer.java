dda02aad2a121d48a9c20c1a9c768538fcbf7e2f
Cedric Röck <git@roecky.net>
2013-04-04 17:35:49 +0200

    add working active-bpel and ode deployers

--- src/main/java/de/uniba/wiai/dsg/betsy/virtual/server/deployers/VirtualizedActiveBpelDeployer.java
+++ src/main/java/de/uniba/wiai/dsg/betsy/virtual/server/deployers/VirtualizedActiveBpelDeployer.java
@@ -1,5 +1,9 @@
 package de.uniba.wiai.dsg.betsy.virtual.server.deployers;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import de.uniba.wiai.dsg.betsy.virtual.common.exceptions.DeployException;
@@ -12,20 +16,102 @@ public class VirtualizedActiveBpelDeployer implements VirtualizedEngineDeployer 
 
 	@Override
 	public String getName() {
-		return "active-bpel_v";
+		return "active_bpel_v";
 	}
 
 	@Override
-	public void deploy(DeployContainer container) {
-		// TODO Auto-generated method stub
-
+	public void deploy(DeployContainer container) throws DeployException {
+		try {
+			File file = new File(container.getDeploymentDir(),
+					container.getFilename());
+			FileUtils.writeByteArrayToFile(file, container.getData());
+		} catch (IOException exception) {
+			throw new DeployException("Couldn't write the container data to "
+					+ "the local disk:", exception);
+		}
 	}
 
 	@Override
 	public void onPostDeployment(DeployContainer container)
 			throws DeployException {
-		// TODO Auto-generated method stub
+		log.info("waiting for the active_bpel_v deployment process to fire");
 
+		boolean fileAvailable = isDeployedFileAvailable(container);
+		long start = -System.currentTimeMillis();
+		int deployTimeout = container.getDeployTimeout();
+
+		while (!fileAvailable
+				&& (System.currentTimeMillis() + start < deployTimeout)) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+			fileAvailable = isDeployedFileAvailable(container);
+		}
+
+		if (!fileAvailable) {
+			// timed out :/
+			throw new DeployException("Process could not be deployed within "
+					+ deployTimeout + "seconds. The operation timed out.");
+		}
+
+		// process is deployed, now wait for verification in logfile
+		boolean logVerification = false;
+		File catalinaLog = new File(container.getEngineLogfileDir(),
+				"aeDeployment.log");
+		String successMessage = "["
+				+ container.getBpelFileNameWithoutExtension() + ".pdd]";
+		int errorCount = 0;
+
+		if (catalinaLog.isFile()) {
+			// verify deployment with engine log. Either until deployment
+			// result or until timeout is reached
+			while (!logVerification
+					&& (System.currentTimeMillis() + start < deployTimeout)) {
+				log.debug("try log verification, wait for: '" + successMessage
+						+ "'");
+				try {
+					String fileContent = FileUtils
+							.readFileToString(catalinaLog);
+					logVerification = fileContent.contains(successMessage);
+					if (!logVerification) {
+						// not available yet? wait a little...
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							// ignore
+						}
+					}
+				} catch (IOException exception) {
+					log.error("Error while reading catalina.out:", exception);
+					if (errorCount > 3) {
+						log.error("Reading catalina.out failed several times"
+								+ ", skip log verification");
+					}
+				}
+			}
+			if (!logVerification) {
+				String msg = "Process could not be deployed within "
+						+ deployTimeout + "seconds. Log verification failed. "
+						+ "The operation timed out.";
+				log.warn(msg);
+				throw new DeployException(msg);
+			}
+		} else {
+			log.warn("aeDeployment.log not found, wait 1s for deployment");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+		}
 	}
 
+	private boolean isDeployedFileAvailable(DeployContainer container) {
+		File file = new File(container.getDeploymentDir() + "/work/ae_temp_"
+				+ container.getBpelFileNameWithoutExtension()
+				+ "_bpr/META-INF/catalog.xml");
+		return file.isFile();
+	}
 }
--- src/main/java/de/uniba/wiai/dsg/betsy/virtual/server/deployers/VirtualizedOrchestraDeployer.java
+++ src/main/java/de/uniba/wiai/dsg/betsy/virtual/server/deployers/VirtualizedOrchestraDeployer.java
@@ -1,5 +1,13 @@
 package de.uniba.wiai.dsg.betsy.virtual.server.deployers;
 
+import groovy.util.AntBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import de.uniba.wiai.dsg.betsy.virtual.common.exceptions.DeployException;
@@ -12,21 +20,47 @@ public class VirtualizedOrchestraDeployer implements VirtualizedEngineDeployer {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "orchestra_v";
 	}
 
 	@Override
-	public void deploy(DeployContainer container) {
-		// TODO Auto-generated method stub
+	public void deploy(DeployContainer container) throws DeployException {
 
+		// TODO write data to temp dir
+		File tmpFile = new File("tmp/" + container.getFilename());
+		createLocalDeployFile(tmpFile, container);
+		log.info("Temporary deploy file written to disk");
+		
+		// TODO requires Ant on system
+		AntBuilder ant = new AntBuilder();
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("executable", "ant");
+		// TODO check where directory is located
+		map.put("dir", "${engine.serverPath}/orchestra-cxf-tomcat-4.9.0");
+		map.put("dir", container.getDeploymentDir());
+		// TODO verify if is working
+		map.put("arg", "deploy");
+		map.put("arg", "-Dbar=" + tmpFile.getAbsolutePath().toString());
+
+		map.put("osfamily", "unix");
+		ant.invokeMethod("exec", map);
+
+	}
+
+	private void createLocalDeployFile(File tmpFile, DeployContainer container) throws DeployException {
+		try {
+			FileUtils.writeByteArrayToFile(tmpFile, container.getData());
+		} catch (IOException exception) {
+			throw new DeployException("Couldn't write the container data to "
+					+ "the local disk:", exception);
+		}
 	}
 
 	@Override
 	public void onPostDeployment(DeployContainer container)
 			throws DeployException {
-		// TODO Auto-generated method stub
-
+		// do nothing - as using synchronous deployment
 	}
 
 }
