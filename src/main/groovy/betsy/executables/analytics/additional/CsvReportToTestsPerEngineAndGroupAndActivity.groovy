a8fc51d616fd7390ccab91880dee016fb7fb7366
Simon Harrer <simon.harrer@gmail.com>
2013-05-02 12:46:44 +0200

    Adds support for creating aggregated statistics out of csv result files

--- src/main/groovy/betsy/executables/analytics/additional/CsvReportToTestsPerEngineAndGroupAndActivity.groovy
+++ src/main/groovy/betsy/executables/analytics/additional/CsvReportToTestsPerEngineAndGroupAndActivity.groovy
@@ -2,9 +2,14 @@ package betsy.executables.analytics.additional
 
 import betsy.executables.analytics.CsvReportLoader
 import betsy.executables.analytics.model.CsvReport
+import betsy.executables.analytics.model.Engine
+import betsy.executables.analytics.model.Group
 import betsy.executables.analytics.model.Result
 
 class CsvReportToTestsPerEngineAndGroupAndActivity {
+
+    String VALUE_DELIMITER = "\t"
+    String ENTRY_DELIMITER = "\n"
 
     CsvReport report
 
@@ -12,12 +17,21 @@ class CsvReportToTestsPerEngineAndGroupAndActivity {
 
     void toCsvReport(PrintStream writer) {
 
-        writer.println "Activity\t" + report.engines.collect {it.name}.join("\t") + "\ttotal tests"
+        writer.print "Activity" + VALUE_DELIMITER + report.engines.collect {it.name}.join(VALUE_DELIMITER) + VALUE_DELIMITER + "total" + ENTRY_DELIMITER
+
+        SortedMap<Engine,Integer> successesPerEngineTotal = new TreeMap<>();
+        int total = 0
 
         report.groups.each { group ->
-            def activityGroups = group.tests.groupBy { properties.get(it.name) }.sort() {it.key}
+            print group.name + VALUE_DELIMITER * (report.engines.size() + 2) + ENTRY_DELIMITER
+
+            def activityGroups = group.tests.groupBy { getGroupByTestName(it.name) }.sort() {it.key}
+
+            SortedMap<Engine,Integer> successesPerEngine = new TreeMap<>();
+            int groupTotal = 0
+
             activityGroups.each { key, values ->
-                writer.print key + "\t"
+                writer.print key + VALUE_DELIMITER
                 report.engines.each { engine ->
                     int successes = 0
                     values.each { test ->
@@ -25,12 +39,31 @@ class CsvReportToTestsPerEngineAndGroupAndActivity {
                             successes++;
                         }
                     }
-                    writer.print successes + "\t"
+                    writer.print successes + VALUE_DELIMITER
+
+                    successesPerEngine.put(engine, successes + successesPerEngine.get(engine, 0))
+                    successesPerEngineTotal.put(engine, successes + successesPerEngineTotal.get(engine, 0))
                 }
+                total += values.size()
+                groupTotal += values.size()
+
                 writer.print values.size()
-                writer.print "\n"
+                writer.print ENTRY_DELIMITER
             }
+            List<Integer> values = successesPerEngine.collect { key, value -> value}
+            print "total" + VALUE_DELIMITER + values.join(VALUE_DELIMITER) + VALUE_DELIMITER + groupTotal + ENTRY_DELIMITER
         }
+        List<Integer> values = successesPerEngineTotal.collect { key, value -> value}
+        writer.print "total" + VALUE_DELIMITER + values.join(VALUE_DELIMITER) + VALUE_DELIMITER + total + ENTRY_DELIMITER
+    }
+
+    private String getGroupByTestName(String testname){
+        String group = properties.get(testname)
+        if(group == null){
+            // Test $testname does not have a group relation. Assuming testname as a group
+            group = testname
+        }
+        return group;
     }
 
     public static void main(String[] args) {
