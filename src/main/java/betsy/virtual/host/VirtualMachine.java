8170a0087f534b40bc28634eac226006bd066e3b
Cedric Röck <git@roecky.net>
2013-05-04 10:11:28 +0200

    VirtualMachine - removed unused imports

--- src/main/java/betsy/virtual/host/VirtualMachine.java
+++ src/main/java/betsy/virtual/host/VirtualMachine.java
@@ -6,13 +6,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
