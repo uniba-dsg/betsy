e8edb330be32f7ef09787a6ea1c4ff7228b182a4
Simon Harrer <simon.harrer@gmail.com>
2013-10-24 15:10:53 +0200

    Remove commented out code

--- src/main/groovy/betsy/data/engines/installer/OrchestraInstaller.groovy
+++ src/main/groovy/betsy/data/engines/installer/OrchestraInstaller.groovy
@@ -28,21 +28,4 @@ class OrchestraInstaller {
         ant.ant target: "install", dir: installDir
     }
 
-    /*
-        <property name="orchestra.dir" value="server/orchestra"/>
-    <property name="orchestra.name" value="orchestra-cxf-tomcat-4.9.0"/>
-    <property name="orchestra.file.name" value="orchestra-cxf-tomcat-4.9.0.zip"/>
-    <property name="orchestra.download.url" value="${download.base.url}${orchestra.file.name}"/>
-
-    <target name="orchestra" description="Install Orchestra">
-
-        <!-- set properties to custom tomcat installation (not the one included) -->
-        <propertyfile file="${orchestra.dir}/${orchestra.name}/conf/install.properties">
-            <entry key="catalina.home" value="../apache-tomcat-7.0.26"/>
-        </propertyfile>
-
-        <!-- configure orchestra to use custom tomcat installation -->
-        <ant target="install" dir="${orchestra.dir}/${orchestra.name}"/>
-    </target>
-     */
 }
