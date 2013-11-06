4e57af52ea262f0da24fa6b5093193055eb19876
Simon Harrer <simon.harrer@gmail.com>
2013-11-06 16:16:29 +0100

    Simplified Main class.

--- src/main/groovy/betsy/Main.groovy
+++ src/main/groovy/betsy/Main.groovy
@@ -1,5 +1,8 @@
 package betsy
 
+import betsy.cli.CliParser
+import betsy.cli.EngineParser
+import betsy.cli.ProcessParser
 import betsy.corebpel.CoreBPELEngineExtension
 import betsy.data.BetsyProcess
 import betsy.data.TestCase
@@ -10,9 +13,6 @@ import betsy.virtual.host.engines.VirtualizedEngine
 import betsy.virtual.host.virtualbox.VBoxConfiguration
 import betsy.virtual.host.virtualbox.VBoxWebService
 import betsy.virtual.host.virtualbox.VirtualBoxImpl
-import betsy.cli.CliParser
-import betsy.cli.EngineParser
-import betsy.cli.ProcessParser
 
 import java.awt.*
 import java.util.List
@@ -30,11 +30,7 @@ class Main {
             System.exit(0)
         }
 
-        // setting partner address
-        if (parser.hasCustomPartnerAddress()) {
-            println "Setting Partner IP and Port to ${parser.getCustomPartnerAddress()} from previous setting ${Configuration.get("partner.ipAndPort")}"
-            Configuration.set("partner.ipAndPort", parser.getCustomPartnerAddress())
-        }
+        customPartnerAddress(parser)
 
         // parsing processes and engines
         List<Engine> engines = null
@@ -49,64 +45,11 @@ class Main {
             System.exit(0)
         }
 
-        // print selection of engines and processes
-        println "Engines: ${engines.collect { it.name }}"
-        println "Processes: ${processes.size() < 10 ? processes.collect { it.bpelFileNameWithoutExtension } : processes.size()}"
+        printSelectedEnginesAndProcesses(engines, processes)
 
-
-        if (parser.checkDeployment()) {
-            // check only whether the processes can be deployed
-            processes.each { process ->
-                process.testCases = [new TestCase().checkDeployment()]
-            }
-        }
-
-        if (parser.transformToCoreBpel()) {
-
-            String transformations = parser.getCoreBPELTransformations()
-
-            if (transformations == "ALL") {
-                for (Engine engine : engines) {
-                    if (engine instanceof VirtualizedEngine) {
-                        CoreBPELEngineExtension.extendEngine(engine.defaultEngine)
-                    } else if (engine instanceof LocalEngine) {
-                        CoreBPELEngineExtension.extendEngine(engine)
-                    }
-                }
-            } else if (transformations == "NONE") {
-                // do nothing - default value
-            } else {
-                String[] xsls = transformations.split(",")
-
-                for (Engine engine : engines) {
-                    if (engine instanceof VirtualizedEngine) {
-                        CoreBPELEngineExtension.extendEngine(engine.defaultEngine, xsls)
-                    } else if (engine instanceof LocalEngine) {
-                        CoreBPELEngineExtension.extendEngine(engine, xsls)
-                    }
-                }
-            }
-
-        }
-
-        if (engines.any { it instanceof VirtualizedEngine }) {
-            // verify IP set
-            String partner = Configuration.get("partner.ipAndPort")
-            if (partner.contains("0.0.0.0") || partner.contains("127.0.0.1")) {
-                throw new IllegalStateException("VirtualizedEngines require your local IP-Address to be set. This can either be done via the -p option or directly in the Config.groovy file.")
-            }
-
-            // verify all mandatory config options
-            new VBoxConfiguration().verify()
-            new VBoxWebService().startAndInstall()
-
-            VirtualBox vb = new VirtualBoxImpl()
-            engines.each {
-                if (it instanceof VirtualizedEngine) {
-                    it.virtualBox = vb
-                }
-            }
-        }
+        checkDeployment(parser, processes)
+        coreBpel(parser, engines)
+        virtualEngines(engines)
 
         Betsy betsy = new Betsy(engines: engines, processes: processes)
 
@@ -133,6 +76,81 @@ class Main {
             // WARNING when a class is not found in soapUI, the corresponding exception does not show up.
             // SOLUTION remove exit line for testing purposes
             System.exit(0)
+        }
+    }
+
+    protected static void printSelectedEnginesAndProcesses(List<Engine> engines, List<BetsyProcess> processes) {
+        // print selection of engines and processes
+        println "Engines: ${engines.collect { it.name }}"
+        println "Processes: ${processes.size() < 10 ? processes.collect { it.bpelFileNameWithoutExtension } : processes.size()}"
+    }
+
+    protected static void customPartnerAddress(CliParser parser) {
+        // setting partner address
+        if (parser.hasCustomPartnerAddress()) {
+            println "Setting Partner IP and Port to ${parser.getCustomPartnerAddress()} from previous setting ${Configuration.get("partner.ipAndPort")}"
+            Configuration.set("partner.ipAndPort", parser.getCustomPartnerAddress())
+        }
+    }
+
+    protected static void checkDeployment(CliParser parser, List<BetsyProcess> processes) {
+        if (parser.checkDeployment()) {
+            // check only whether the processes can be deployed
+            processes.each { process ->
+                process.testCases = [new TestCase().checkDeployment()]
+            }
+        }
+    }
+
+    protected static void virtualEngines(List<Engine> engines) {
+        if (engines.any { it instanceof VirtualizedEngine }) {
+            // verify IP set
+            String partner = Configuration.get("partner.ipAndPort")
+            if (partner.contains("0.0.0.0") || partner.contains("127.0.0.1")) {
+                throw new IllegalStateException("VirtualizedEngines require your local IP-Address to be set. " +
+                        "This can either be done via the -p option or directly in the Config.groovy file.")
+            }
+
+            // verify all mandatory config options
+            new VBoxConfiguration().verify()
+            new VBoxWebService().startAndInstall()
+
+            VirtualBox vb = new VirtualBoxImpl()
+            engines.each {
+                if (it instanceof VirtualizedEngine) {
+                    it.virtualBox = vb
+                }
+            }
+        }
+    }
+
+    protected static void coreBpel(CliParser parser, List<Engine> engines) {
+        if (parser.transformToCoreBpel()) {
+
+            String transformations = parser.getCoreBPELTransformations()
+
+            if (transformations == "ALL") {
+                for (Engine engine : engines) {
+                    if (engine instanceof VirtualizedEngine) {
+                        CoreBPELEngineExtension.extendEngine(engine.defaultEngine)
+                    } else if (engine instanceof LocalEngine) {
+                        CoreBPELEngineExtension.extendEngine(engine)
+                    }
+                }
+            } else if (transformations == "NONE") {
+                // do nothing - default value
+            } else {
+                String[] xsls = transformations.split(",")
+
+                for (Engine engine : engines) {
+                    if (engine instanceof VirtualizedEngine) {
+                        CoreBPELEngineExtension.extendEngine(engine.defaultEngine, xsls)
+                    } else if (engine instanceof LocalEngine) {
+                        CoreBPELEngineExtension.extendEngine(engine, xsls)
+                    }
+                }
+            }
+
         }
     }
 
