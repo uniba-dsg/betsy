827286975c46c92603a226928991356feae13bf6
Simon Harrer <simon.harrer@gmail.com>
2013-11-07 10:40:27 +0100

    Make ServiceAddress immutable.

--- src/main/groovy/betsy/virtual/host/ServiceAddress.java
+++ src/main/groovy/betsy/virtual/host/ServiceAddress.java
@@ -1,63 +1,54 @@
 package betsy.virtual.host;
 
-import org.apache.commons.lang.StringUtils;
-
 import betsy.data.engines.Engine;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * A {@link ServiceAddress} is an address to a service that is mandatory for the
  * proper execution of an {@link Engine}. It is also possible to specify special
  * content that must be available on this address to show that the service is
  * available and ready for usage.
- * 
+ *
  * @author Cedric Roeck
  * @version 1.0
  */
 public class ServiceAddress {
 
-	private String address;
-	private String requiredHtmlContent;
+    private final String address;
+    private final String requiredHtmlContent;
 
-	public ServiceAddress(final String address, final String requiredHtmlContent) {
-		this.setAddress(address);
-		this.setRequiredHtmlContent(requiredHtmlContent);
-	}
+    public ServiceAddress(final String address, final String requiredHtmlContent) {
+        if (StringUtils.isBlank(address)) {
+            throw new IllegalArgumentException("Address must not be blank");
+        }
 
-	public ServiceAddress(final String address) {
-		this(address, null);
-	}
+        if (requiredHtmlContent == null) {
+            throw new IllegalArgumentException("html content must not be null");
+        }
 
-	@Override
-	public String toString() {
-		return address;
-	}
+        this.address = address;
+        this.requiredHtmlContent = requiredHtmlContent;
+    }
 
-	public void setAddress(final String address) {
-		if (StringUtils.isBlank(address)) {
-			throw new IllegalArgumentException("Address must not be blank");
-		}
-		this.address = address;
-	}
+    public ServiceAddress(final String address) {
+        this(address, "");
+    }
 
-	public void setRequiredHtmlContent(final String requiredContent) {
-		if (StringUtils.isBlank(requiredContent)) {
-            // TODO warum hier keine exception, bei der Adresse aber schon?
-			this.requiredHtmlContent = "";
-		} else {
-			this.requiredHtmlContent = requiredContent;
-		}
-	}
+    @Override
+    public String toString() {
+        return address;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public boolean isRequiringHtmlContent() {
-		return !requiredHtmlContent.isEmpty();
-	}
+    public boolean isRequiringHtmlContent() {
+        return !requiredHtmlContent.isEmpty();
+    }
 
-	public String getRequiredHtmlContent() {
-		return requiredHtmlContent;
-	}
+    public String getRequiredHtmlContent() {
+        return requiredHtmlContent;
+    }
 
 }
