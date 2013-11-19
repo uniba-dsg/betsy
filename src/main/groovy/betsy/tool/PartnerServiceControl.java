16d8794810fa52595bee232d6729012923cf9f20
Simon Harrer <simon.harrer@gmail.com>
2013-11-19 12:15:58 +0100

    Convert PartnerServiceControl to Java.

---
+++ src/main/groovy/betsy/tool/PartnerServiceControl.java
@@ -0,0 +1,59 @@
+package betsy.tool;
+
+import betsy.executables.ws.TestPartnerServicePublisher;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PartnerServiceControl extends JFrame {
+
+    public static void main(String[] args) {
+        new PartnerServiceControl().setVisible(true);
+    }
+
+    private final TestPartnerServicePublisher publisher;
+
+    public PartnerServiceControl() {
+        layoutFrame();
+        publisher = new TestPartnerServicePublisher();
+
+        createStartButton();
+
+        createStopButton();
+    }
+
+    private void layoutFrame() {
+        this.setLayout(new FlowLayout());
+        this.setSize(300, 75);
+        this.setTitle("Partner Service Control Center");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void createStartButton() {
+        JButton start = new JButton("startup");
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                publisher.publish();
+            }
+
+        });
+        this.add(start);
+    }
+
+    private void createStopButton() {
+        JButton stop = new JButton("stop");
+        stop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                publisher.unpublish();
+            }
+
+        });
+        this.add(stop);
+    }
+
+
+}
