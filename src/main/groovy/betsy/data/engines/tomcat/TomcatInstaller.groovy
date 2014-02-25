uniba-dsg/betsy

commit 9f5d18b2af7991ea9ccb1cd7604063a68fac8831

Fixes definition of CATALINA_OPTS variable

src/main/groovy/betsy/data/engines/tomcat/TomcatInstaller.groovy
@@ -33,7 +33,7 @@ class TomcatInstaller {
 cd ${tomcatBinFolder.toAbsolutePath()} && call startup.bat""")
         FileTasks.createFile(destinationDir.resolve("tomcat_shutdown.bat"), "cd ${tomcatBinFolder.toAbsolutePath()} && call shutdown.bat")
 
-        FileTasks.createFile(destinationDir.resolve("tomcat_startup.sh"), """CATALINA_OPTS=-Xmx3048M -XX:MaxPermSize=2048m ${additionalVmParam}
+        FileTasks.createFile(destinationDir.resolve("tomcat_startup.sh"), """CATALINA_OPTS=\"-Xmx3048M -XX:MaxPermSize=2048m\" ${additionalVmParam}
 cd ${tomcatBinFolder.toAbsolutePath()} && ./startup.sh""")
         FileTasks.createFile(destinationDir.resolve("tomcat_shutdown.sh"), "cd ${tomcatBinFolder.toAbsolutePath()} && ./shutdown.sh")
 

commit 02b319fe3a30f1831c5bcfbd1bc89ccc693de769

Fixes issues introduced in 9f5d18b2af7991ea9ccb1cd7604063a68fac8831 when using additional VM params

src/main/groovy/betsy/data/engines/tomcat/TomcatInstaller.groovy
@@ -33,7 +33,7 @@ class TomcatInstaller {
 cd ${tomcatBinFolder.toAbsolutePath()} && call startup.bat""")
         FileTasks.createFile(destinationDir.resolve("tomcat_shutdown.bat"), "cd ${tomcatBinFolder.toAbsolutePath()} && call shutdown.bat")
 
-        FileTasks.createFile(destinationDir.resolve("tomcat_startup.sh"), """CATALINA_OPTS=\"-Xmx3048M -XX:MaxPermSize=2048m\" ${additionalVmParam}
+        FileTasks.createFile(destinationDir.resolve("tomcat_startup.sh"), """CATALINA_OPTS=\"-Xmx3048M -XX:MaxPermSize=2048m ${additionalVmParam}\"
 cd ${tomcatBinFolder.toAbsolutePath()} && ./startup.sh""")
         FileTasks.createFile(destinationDir.resolve("tomcat_shutdown.sh"), "cd ${tomcatBinFolder.toAbsolutePath()} && ./shutdown.sh")
 