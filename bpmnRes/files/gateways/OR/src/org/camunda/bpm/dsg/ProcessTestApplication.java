337dc56456d419b46491b26fe890d7753270927f
Mathias Casar <mathias.casar@yahoo.de>
2014-03-13 12:10:15 +0100

    added OR Test Case

---
+++ bpmnRes/files/gateways/OR/src/org/camunda/bpm/dsg/ProcessTestApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.dsg;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Test Application")
+public class ProcessTestApplication extends ServletProcessApplication{
+	//empty implementation
+}
