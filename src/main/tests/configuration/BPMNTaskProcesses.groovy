8ef062bdb6d6d8447d8a89e65bedb4fe28c2b8bf
Andreas Vorndran <andreas-vorndran@web.de>
2014-03-25 10:30:04 +0100

    added multi parallel task

--- src/main/tests/configuration/BPMNTaskProcesses.groovy
+++ src/main/tests/configuration/BPMNTaskProcesses.groovy
@@ -14,7 +14,14 @@ class BPMNTaskProcesses {
     )
 
     public static final BPMNProcess MULTI_SEQUENTIAL = builder.buildTaskProcess(
-            "MultiSequencialTask", "de.uniba.dsg", "1.0", "A simple Test for a 3 times sequentially looped script task",
+            "MultiSequencialTask", "de.uniba.dsg", "1.0", "A simple Test for a 3 times sequentially instantiated script task",
+            [
+                    new BPMNTestCase().buildMulti3()
+            ]
+    )
+
+    public static final BPMNProcess MULTI_PARALLEL = builder.buildTaskProcess(
+            "MultiParallelTask", "de.uniba.dsg", "1.0", "A simple Test for a 3 times parallel instantiated script task",
             [
                     new BPMNTestCase().buildMulti3()
             ]
@@ -22,6 +29,7 @@ class BPMNTaskProcesses {
 
     public static final List<BPMNProcess> TASKS = [
             SIMPLE,
-            MULTI_SEQUENTIAL
+            MULTI_SEQUENTIAL,
+            MULTI_PARALLEL
     ].flatten() as List<BPMNProcess>
 }
