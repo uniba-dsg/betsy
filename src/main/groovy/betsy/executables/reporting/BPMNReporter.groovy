bf16ef561ea2eb63afbef2b06565c06a18f1a6e2
Andreas Vorndran <andreas-vorndran@web.de>
2014-03-26 18:06:56 +0100

    improved unit test html output with process image

--- src/main/groovy/betsy/executables/reporting/BPMNReporter.groovy
+++ src/main/groovy/betsy/executables/reporting/BPMNReporter.groovy
@@ -8,5 +8,6 @@ class BPMNReporter {
     void createReports(){
         new JUnitHtmlReports(path: tests.path).create()
         new JUnitXmlResultToCsvRow(xml: tests.JUnitXMLFilePath, csv: tests.csvFilePath).create()
+        new BPMNUnitTestImprover(suite: tests).addProcessImagesToUnitReports()
     }
 }
