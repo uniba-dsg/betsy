97c956b4918ec3bb96949b725e24269b39f7fc8d
Simon Harrer <simon.harrer@gmail.com>
2014-05-28 16:18:27 +0200

    Ensures that the folder to download the file to is available

--- src/main/groovy/betsy/tasks/NetworkTasks.groovy
+++ src/main/groovy/betsy/tasks/NetworkTasks.groovy
@@ -3,12 +3,18 @@ package betsy.tasks
 import ant.tasks.AntUtil
 import betsy.config.Configuration
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
 class NetworkTasks {
 
     public static void downloadFile(URL url, Path fileOrFolder) {
+        // ensure that the folders in this path are created
+        if(Files.isDirectory(fileOrFolder)) {
+            FileTasks.mkdirs(fileOrFolder);
+        }
+
         AntUtil.builder().get(dest: fileOrFolder, src: url, skipexisting: true);
     }
 
