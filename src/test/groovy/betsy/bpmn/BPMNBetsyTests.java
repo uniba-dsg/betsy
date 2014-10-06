ea78ab89259b42d4ce0bf6d26bd82eeda0293b5b
Simon Harrer <simon.harrer@gmail.com>
2014-10-06 17:22:40 +0200

    Create tests for BPMN

---
+++ src/test/groovy/betsy/bpmn/BPMNBetsyTests.java
@@ -0,0 +1,79 @@
+package betsy.bpmn;
+
+import betsy.bpel.Betsy;
+import betsy.bpel.Composite;
+import betsy.bpel.engines.Engine;
+import betsy.bpel.engines.EnginePackageBuilder;
+import betsy.bpel.model.BetsyProcess;
+import betsy.bpmn.engines.BPMNEngine;
+import betsy.bpmn.model.BPMNProcess;
+import configuration.bpel.ProcessRepository;
+import configuration.bpmn.BPMNProcessRepository;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class BPMNBetsyTests {
+    @Test
+    public void simulateATestRun() throws Exception {
+        BPMNEngine engine = new MockEngine();
+        List<BPMNProcess> processes = new BPMNProcessRepository().getByName("ALL");
+        BPMNBetsy betsy = new BPMNBetsy();
+
+
+        betsy.setEngines(Arrays.asList(engine));
+        betsy.setProcesses(processes);
+        betsy.execute();
+    }
+
+    public class MockEngine extends BPMNEngine {
+        public void install() {
+        }
+
+        public void startup() {
+        }
+
+        public void shutdown() {
+        }
+
+        public boolean isRunning() {
+            return false;
+        }
+
+        public void deploy(BPMNProcess process) {
+        }
+
+        public void storeLogs(BPMNProcess process) {
+        }
+
+        public void buildArchives(BPMNProcess process) {
+
+        }
+
+        public String getEndpointUrl(BPMNProcess process) {
+            return "myendpoint";
+        }
+
+        public String getName() {
+            return "mock";
+        }
+
+        public Path getXsltPath() {
+            return Paths.get("unused_path");
+        }
+
+        @Override
+        public void buildTest(BPMNProcess process) {
+
+        }
+
+        @Override
+        public void testProcess(BPMNProcess process) {
+
+        }
+
+    }
+}
