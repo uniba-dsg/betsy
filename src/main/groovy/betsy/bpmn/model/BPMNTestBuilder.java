85934dbd99b17188c6bbc0cf3bb463c16d5380f0
Simon Harrer <simon.harrer@gmail.com>
2014-10-10 12:02:17 +0200

    Convert to java plus minor improvements.

---
+++ src/main/groovy/betsy/bpmn/model/BPMNTestBuilder.java
@@ -0,0 +1,76 @@
+package betsy.bpmn.model;
+
+import betsy.common.tasks.FileTasks;
+import betsy.common.util.ClasspathHelper;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class BPMNTestBuilder {
+    public void buildTests() {
+        //Build test for each Test Case
+        for (BPMNTestCase testCase : process.getTestCases()) {
+
+            String logFilePath = logDir.resolve("log" + String.valueOf(testCase.getNumber()) + ".txt").toUri().toString().substring(8);
+
+            //assemble array of assertion for unitTestString
+            String assertionListString = getAssertionString(testCase.getAssertions());
+
+            Path testClass = process.getTargetTestSrcPathWithCase(testCase.getNumber()).resolve(packageString.replace(".", "/")).resolve(process.getName() + ".java");
+
+            FileTasks.copyFileContentsToNewFile(ClasspathHelper.getFilesystemPathFromClasspathPath("/bpmn/ProcessTestClass.template"), testClass);
+
+            HashMap<String, String> replacements = new HashMap<String, String>();
+            replacements.put("PROCESS_NAME", process.getName());
+            replacements.put("PACKAGE_STRING", packageString);
+            replacements.put("TEST_CASE", testCase.toString());
+            replacements.put("ASSERTION_LIST_STRING", assertionListString);
+            replacements.put("LOGFILE_PATH", logFilePath);
+
+            FileTasks.replaceTokensInFile(testClass, replacements);
+        }
+
+    }
+
+    public static String getAssertionString(List<String> assertions) {
+        StringJoiner joiner = new StringJoiner(",", "{", "}");
+
+        for (String string : assertions) {
+            joiner.add(ESCAPED_DOUBLE_QUOTATION_MARK + string + ESCAPED_DOUBLE_QUOTATION_MARK);
+        }
+
+
+        return joiner.toString();
+    }
+
+    public String getPackageString() {
+        return packageString;
+    }
+
+    public void setPackageString(String packageString) {
+        this.packageString = packageString;
+    }
+
+    public Path getLogDir() {
+        return logDir;
+    }
+
+    public void setLogDir(Path logDir) {
+        this.logDir = logDir;
+    }
+
+    public BPMNProcess getProcess() {
+        return process;
+    }
+
+    public void setProcess(BPMNProcess process) {
+        this.process = process;
+    }
+
+    public static final String ESCAPED_DOUBLE_QUOTATION_MARK = "\"";
+    private String packageString;
+    private Path logDir;
+    private BPMNProcess process;
+}
---
+++ src/main/groovy/betsy/bpmn/model/BPMNTestCase.java
@@ -0,0 +1,199 @@
+package betsy.bpmn.model;
+
+import betsy.common.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BPMNTestCase {
+    public BPMNTestCase() {
+        this(1);
+    }
+
+    public BPMNTestCase(int number) {
+        this.number = number;
+        initializeTestCaseNumber();
+    }
+
+    private void initializeTestCaseNumber() {
+        variables.add(new BPMNTestCaseVariable("testCaseNumber", "Integer", number));
+    }
+
+    private BPMNTestCase addInputTestString(String value) {
+        variables.add(new BPMNTestCaseVariable("test", "String", value));
+
+        return this;
+    }
+
+    public BPMNTestCase inputA() {
+        return addInputTestString("a");
+    }
+
+    public BPMNTestCase inputB() {
+        return addInputTestString("b");
+    }
+
+    public BPMNTestCase inputAB() {
+        return addInputTestString("ab");
+    }
+
+    public BPMNTestCase inputC() {
+        return addInputTestString("c");
+    }
+
+    private BPMNTestCase addAssertions(String assertion) {
+        assertions.add(assertion);
+        return this;
+    }
+
+    public BPMNTestCase assertSuccess() {
+        return addAssertions("success");
+    }
+
+    public BPMNTestCase assertTask1() {
+        return addAssertions("task1");
+    }
+
+    public BPMNTestCase assertTask2() {
+        return addAssertions("task2");
+    }
+
+    public BPMNTestCase assertTrue() {
+        return addAssertions("true");
+    }
+
+    public BPMNTestCase assertFalse() {
+        return addAssertions("false");
+    }
+
+    public BPMNTestCase assertDefault() {
+        return addAssertions("default");
+    }
+
+    public BPMNTestCase assertRuntimeException() {
+        return addAssertions("runtimeException");
+    }
+
+    public BPMNTestCase assertThrownErrorEvent() {
+        return addAssertions("thrownErrorEvent");
+    }
+
+    public BPMNTestCase assertSubprocess() {
+        return addAssertions("subprocess");
+    }
+
+    public BPMNTestCase assertLane1() {
+        return addAssertions("lane1");
+    }
+
+    public BPMNTestCase assertLane2() {
+        return addAssertions("lane2");
+    }
+
+    public BPMNTestCase assertMulti() {
+        return addAssertions("multi");
+    }
+
+    public BPMNTestCase assertInterrupted() {
+        return addAssertions("interrupted");
+    }
+
+    public BPMNTestCase assertSignaled() {
+        return addAssertions("signaled");
+    }
+
+    public BPMNTestCase assertNormalTask() {
+        return addAssertions("normalTask");
+    }
+
+    public BPMNTestCase assertErrorTask() {
+        return addAssertions("errorTask");
+    }
+
+    public BPMNTestCase assertTransactionTask() {
+        return addAssertions("transaction");
+    }
+
+    public BPMNTestCase assertCompensate() {
+        return addAssertions("compensate");
+    }
+
+    public BPMNTestCase assertNotInterrupted() {
+        return addAssertions("notInterrupted");
+    }
+
+    public BPMNTestCase assertStarted() {
+        return addAssertions("started");
+    }
+
+    public BPMNTestCase assertTimerInternal() {
+        return addAssertions("timerInternal");
+    }
+
+    public BPMNTestCase assertTimerExternal() {
+        return addAssertions("timerExternal");
+    }
+
+    public BPMNTestCase assertTimerEvent() {
+        return addAssertions("timerEvent");
+    }
+
+    public BPMNTestCase assertCalled() {
+        return addAssertions("called");
+    }
+
+    public BPMNTestCase assertCondition() {
+        return addAssertions("condition");
+    }
+
+    public BPMNTestCase optionDelay(int delay) {
+        this.delay = delay;
+        return this;
+    }
+
+    public BPMNTestCase optionSelfStarting() {
+        selfStarting = true;
+        return this;
+    }
+
+    public boolean getSelfStarting() {
+        return selfStarting;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public List<String> getAssertions() {
+        return assertions;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("test").append(number).append("Assert");
+        for (String assertion : assertions) {
+            sb.append(StringUtils.capitalize(assertion));
+        }
+        return sb.toString();
+    }
+
+    public List<BPMNTestCaseVariable> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(List<BPMNTestCaseVariable> variables) {
+        this.variables = variables;
+    }
+
+    private int number;
+    private List<String> assertions = new ArrayList<>();
+    private boolean selfStarting = false;
+    private int delay = 0;
+    private List<BPMNTestCaseVariable> variables = new LinkedList<>();
+}
