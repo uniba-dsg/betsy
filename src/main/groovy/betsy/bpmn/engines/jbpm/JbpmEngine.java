8d3d3d052488d1f89fb091a49903c31c64703b68
Simon Harrer <simon.harrer@gmail.com>
2014-10-10 15:12:42 +0200

    Convert to java

---
+++ src/main/groovy/betsy/bpmn/engines/jbpm/JbpmEngine.java
@@ -0,0 +1,185 @@
+package betsy.bpmn.engines.jbpm;
+
+import betsy.bpmn.engines.BPMNEngine;
+import betsy.bpmn.model.BPMNProcess;
+import betsy.bpmn.model.BPMNTestBuilder;
+import betsy.bpmn.model.BPMNTestCase;
+import betsy.bpmn.reporting.BPMNTestcaseMerger;
+import betsy.common.config.Configuration;
+import betsy.common.tasks.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JbpmEngine extends BPMNEngine {
+    @Override
+    public String getName() {
+        return "jbpm";
+    }
+
+    public String getJbpmnUrl() {
+        return "http://localhost:8080/jbpm-console";
+    }
+
+    public String getSystemURL() {
+        return "ssh://admin@localhost:8001/system";
+    }
+
+    public String getJbossName() {
+        return "jboss-as-7.1.1.Final";
+    }
+
+    public Path getJbossStandaloneDir() {
+        return getServerPath().resolve(getJbossName()).resolve("standalone");
+    }
+
+    public String getHomeDir() {
+        String homeDir = System.getenv("HOME");
+        if (homeDir == null) {
+            homeDir = System.getProperty("user.home");
+        }
+
+        return homeDir;
+    }
+
+    public Path getAntPath() {
+        return Configuration.getAntHome().resolve("bin");
+    }
+
+    @Override
+    public void deploy(final BPMNProcess process) {
+        // maven deployment for pushing it to the local maven repository (jbpm-console will fetch it from there)
+        final Path mavenPath = Paths.get("maven/apache-maven-3.2.1/bin");
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(process.getTargetPath().resolve("project"), String.valueOf(mavenPath.toAbsolutePath()) + "/mvn -q clean install"));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(process.getTargetPath().resolve("project"), String.valueOf(mavenPath.toAbsolutePath()) + "/mvn -q clean install"));
+
+        //wait for maven to deploy
+        WaitTasks.sleep(1500);
+
+        //preparing ssh
+        //delete known_hosts file for do not getting trouble with changing remote finger print
+        //FileTasks.deleteFile(Paths.get(homeDir + "/.ssh/known_hosts"))
+        FileTasks.createFile(Paths.get(getHomeDir() + "/.ssh/config"), "Host localhost\n    StrictHostKeyChecking no");
+
+        //deploy by creating a deployment unit, which can be started
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(Paths.get("jbpmdeployer/JPBM-Deployer-1.1"), "java -jar Jbpm-deployer-1.1.jar " + process.getGroupId() + " " + process.getName() + " " + process.getVersion() + " " + getSystemURL()));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(Paths.get("jbpmdeployer/JPBM-Deployer-1.1"), "java -jar Jbpm-deployer-1.1.jar " + process.getGroupId() + " " + process.getName() + " " + process.getVersion() + " " + getSystemURL()));
+
+        //waiting for the result of the deployment
+        WaitTasks.waitForSubstringInFile(20000, 1000, getJbossStandaloneDir().resolve("log").resolve("server.log"), "de.uniba.dsg");
+    }
+
+    @Override
+    public void buildArchives(final BPMNProcess process) {
+
+        XSLTTasks.transform(getXsltPath().resolve("../scriptTask.xsl"), process.getResourcePath().resolve(process.getName() + ".bpmn"), process.getTargetPath().resolve("project/src/main/resources/" + process.getName() + ".bpmn2-temp"));
+        XSLTTasks.transform(getXsltPath().resolve("jbpm.xsl"), process.getTargetPath().resolve("project/src/main/resources/" + process.getName() + ".bpmn2-temp"), process.getTargetPath().resolve("project/src/main/resources/" + process.getName() + ".bpmn2"));
+        FileTasks.deleteFile(process.getTargetPath().resolve("war/WEB-INF/classes/" + process.getName() + ".bpmn2-temp"));
+
+        JbpmResourcesGenerator generator = new JbpmResourcesGenerator();
+
+
+        generator.setJbpmSrcDir(Paths.get("src/main/tests/files/bpmnRes/jbpm"));
+        generator.setDestDir(process.getTargetPath().resolve("project"));
+        generator.setProcessName(process.getName());
+        generator.setGroupId(process.getGroupId());
+        generator.setVersion(process.getVersion());
+        generator.generateProject();
+    }
+
+    @Override
+    public String getEndpointUrl(BPMNProcess process) {
+        return "http://localhost:8080/jbpm-console/";
+    }
+
+    @Override
+    public void storeLogs(BPMNProcess process) {
+        FileTasks.mkdirs(process.getTargetLogsPath());
+
+        // TODO only copy log files from tomcat, the other files are files for the test
+        FileTasks.copyFilesInFolderIntoOtherFolder(getJbossStandaloneDir().resolve("log"), process.getTargetLogsPath());
+
+        for (BPMNTestCase tc : process.getTestCases()) {
+            FileTasks.copyFileIntoFolder(getServerPath().resolve("log" + tc.getNumber() + ".txt"), process.getTargetLogsPath());
+        }
+
+    }
+
+    @Override
+    public void install() {
+        JbpmInstaller installer = new JbpmInstaller();
+        installer.setDestinationDir(getServerPath());
+        installer.install();
+    }
+
+    @Override
+    public void startup() {
+        Path pathToJava7 = Configuration.getJava7Home();
+        FileTasks.assertDirectory(pathToJava7);
+
+        Map<String, String> map = new LinkedHashMap<String, String>(1);
+        map.put("JAVA_HOME", pathToJava7.toString());
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q start.demo.noeclipse"), map);
+        Map<String, String> map1 = new LinkedHashMap<String, String>(1);
+        map1.put("JAVA_HOME", pathToJava7.toString());
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q start.demo.noeclipse"), map1);
+
+        //waiting for jbpm-console for deployment and instantiating
+        WaitTasks.waitForSubstringInFile(120000, 5000, getJbossStandaloneDir().resolve("log").resolve("server.log"), "JBAS018559: Deployed \"jbpm-console.war\"");
+    }
+
+    @Override
+    public void shutdown() {
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q stop.demo"));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q stop.demo"));
+        //waiting for shutdown
+        WaitTasks.sleep(5000);
+        // clean up data (with db and config files in the users home directory)
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q clean.demo"));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), String.valueOf(getAntPath().toAbsolutePath()) + "/ant -q clean.demo"));
+    }
+
+    @Override
+    public boolean isRunning() {
+        return URLTasks.isUrlAvailable(getJbpmnUrl());
+    }
+
+    public void testProcess(final BPMNProcess process) {
+        for (BPMNTestCase testCase : process.getTestCases()) {
+            JbpmTester tester = new JbpmTester();
+            tester.setTestCase(testCase);
+            tester.setName(process.getName());
+            tester.setDeploymentId(process.getGroupId() + ":" + process.getName() + ":" + process.getVersion());
+            tester.setBaseUrl(getEndpointUrlAsUrl(process));
+            tester.setTestSrc(process.getTargetTestSrcPathWithCase(testCase.getNumber()));
+            tester.setReportPath(process.getTargetReportsPathWithCase(testCase.getNumber()));
+            tester.setTestBin(process.getTargetTestBinPathWithCase(testCase.getNumber()));
+            tester.setLogDir(getServerPath());
+            tester.runTest();
+        }
+
+
+        new BPMNTestcaseMerger(process.getTargetReportsPath()).mergeTestCases();
+    }
+
+    private URL getEndpointUrlAsUrl(BPMNProcess process) {
+        try {
+            return new URL(getEndpointUrl(process));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("malformed URL", e);
+        }
+    }
+
+    public void buildTest(final BPMNProcess process) {
+        BPMNTestBuilder builder = new BPMNTestBuilder();
+        builder.setPackageString(getName() + "." + process.getGroup());
+        builder.setLogDir(getServerPath());
+        builder.setProcess(process);
+        builder.buildTests();
+    }
+
+}
