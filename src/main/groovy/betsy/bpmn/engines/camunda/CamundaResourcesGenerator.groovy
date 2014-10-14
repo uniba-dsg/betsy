21cc588510247a5bd92700421d8f363a6ae825a4
Simon Harrer <simon.harrer@gmail.com>
2014-10-14 17:41:23 +0200

    Remove Groovy Ant.

--- src/main/groovy/betsy/bpmn/engines/camunda/CamundaResourcesGenerator.groovy
+++ src/main/groovy/betsy/bpmn/engines/camunda/CamundaResourcesGenerator.groovy
@@ -6,6 +6,11 @@ import betsy.common.config.Configuration
 import betsy.common.tasks.FileTasks
 import betsy.common.tasks.NetworkTasks
 import betsy.common.util.ClasspathHelper
+import org.apache.tools.ant.BuildException
+import org.apache.tools.ant.taskdefs.Javac
+import org.apache.tools.ant.taskdefs.War
+import org.apache.tools.ant.types.FileSet
+import org.apache.tools.ant.types.selectors.FileSelector
 import org.codehaus.groovy.tools.RootLoader
 
 import java.nio.file.Path
@@ -46,16 +51,51 @@ class CamundaResourcesGenerator {
 
         // generate and compile sources
         generateServletProcessApplication(srcDestDir)
-        ant.javac(srcdir: srcDestDir, destdir: classesDir, includeantruntime: false, source: "1.7", target: "1.7") {
-            classpath{
-                pathelement(location: Configuration.getDownloadsDir().resolve("camunda-engine-7.0.0-Final.jar"))
-                pathelement(location: Configuration.getDownloadsDir().resolve("javaee-api-7.0.jar"))
-            }
-        }
-        //pack war
-        ant.war(destfile: destDir.resolve("${processName}.war"), needxmlfile: false){
-            fileset(dir: destDir){ }
-        }
+        compileServletProcessApplication(srcDestDir, classesDir)
+
+        createWar()
+    }
+
+    private void createWar() {
+        War war = new War();
+        war.setDestFile(destDir.resolve("${processName}.war").toFile());
+        war.setNeedxmlfile(false);
+
+        FileSet set = new FileSet()
+        set.setDir(destDir.toFile())
+        war.add(set)
+
+        war.setTaskName("war");
+        war.setProject(AntUtil.builder().getProject());
+
+        war.execute();
+    }
+
+    private void compileServletProcessApplication(Path srcDestDir, Path classesDir) {
+        Javac javac = new Javac();
+
+        org.apache.tools.ant.types.Path sourceDirPath = new org.apache.tools.ant.types.Path(AntUtil.builder().getProject());
+        sourceDirPath.setLocation(srcDestDir.toFile())
+
+        javac.setSrcdir(sourceDirPath);
+        javac.setDestdir(classesDir.toFile());
+        javac.setIncludeantruntime(false);
+        javac.setSource("1.7");
+        javac.setTarget("1.7");
+
+        org.apache.tools.ant.types.Path path = new org.apache.tools.ant.types.Path(AntUtil.builder().getProject());
+        org.apache.tools.ant.types.Path camunda = new org.apache.tools.ant.types.Path(AntUtil.builder().getProject());
+        camunda.setLocation(Configuration.getDownloadsDir().resolve("camunda-engine-7.0.0-Final.jar").toFile());
+        org.apache.tools.ant.types.Path jee = new org.apache.tools.ant.types.Path(AntUtil.builder().getProject());
+        jee.setLocation(Configuration.getDownloadsDir().resolve("javaee-api-7.0.jar").toFile())
+        path.add(camunda)
+        path.add(jee);
+        javac.setClasspath(path);
+
+        javac.setTaskName("javac");
+        javac.setProject(AntUtil.builder().getProject());
+
+        javac.execute();
     }
 
     private void generatePom(Path pomDir){
