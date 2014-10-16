a9cc861b9284485f55f7e49035af747a2589b4a6
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-10-16 13:55:55 +0200

    Document special case

--- src/main/tests/configuration/bpmn/BPMNTaskProcesses.groovy
+++ src/main/tests/configuration/bpmn/BPMNTaskProcesses.groovy
@@ -36,7 +36,8 @@ class BPMNTaskProcesses {
     public static final BPMNProcess SEQUENCE_FLOW_CONDITIONAL_DEFAULT_NORMAL = builder.buildTaskProcess(
             "SequenceFlowConditionalDefaultNormal", "A process with four scriptTasks connected by sequenceFlows. " +
             "The first scriptTask points to the other three tasks with sequenceFlows. " +
-            "The first of these sequenceFlows is associated with a conditionExpression, the second one is marked as default and the third has no condition associated.",
+            "The first of these sequenceFlows is associated with a conditionExpression, the second one is marked as default and the third has no condition associated. " +
+            "This is a special case document in Sec. 13.2.1, p. 427.",
             [
                     new BPMNTestCase(1).inputA().assertTask1().assertTask3(),
                     new BPMNTestCase(2).inputB().assertTask2().assertTask3()
