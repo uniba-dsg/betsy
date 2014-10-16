542f23301bf3580f142806975581cddfe673b06b
Simon Harrer <simon.harrer@gmail.com>
2014-10-16 16:37:31 +0200

    EngineControl can now start BPMN engines as well. Reuses UBML Control Center, except for log file access.

--- src/main/groovy/betsy/bpel/tools/EngineControl.java
+++ src/main/groovy/betsy/bpel/tools/EngineControl.java
@@ -1,145 +1,130 @@
 package betsy.bpel.tools;
 
-import betsy.bpel.engines.Engine;
-import betsy.common.engines.EngineAPI;
 import betsy.bpel.repositories.EngineRepository;
+import betsy.bpmn.repositories.BPMNEngineRepository;
+import betsy.common.engines.EngineLifecycle;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * The GUI to install, start and stop a local engine or all local engines.
  */
 public class EngineControl extends JFrame {
-    public static void main(String[] args) {
+
+    private final DefaultListModel<String> actions = new DefaultListModel<>();
+
+    private EngineControl() {
+
+
+        this.setLayout(new BorderLayout());
+        this.add(createCenterPanel(), BorderLayout.CENTER);
+        JList<String> comp = new JList<>(actions);
+        comp.setVisibleRowCount(4);
+        this.add(new JScrollPane(comp), BorderLayout.NORTH);
+
+        this.setSize(800, 1000);
+        this.setTitle("ECC - Engine Control Center");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        toast("STARTED");
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+
+        UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+
         new EngineControl().setVisible(true);
     }
 
-    public EngineControl() {
-        final List<Engine> engineList = new EngineRepository().getByName("LOCALS");
-        final List<EngineAPI> engines = new LinkedList<>();
-        for (Engine engine : engineList) {
-            engines.add(engine);
+    private JPanel createCenterPanel() {
+        JPanel panel = new JPanel();
+
+        final List<EngineLifecycle> bpelEngines = new EngineRepository().getByName("ALL").stream().collect(Collectors.toList());
+        final List<EngineLifecycle> bpmnEngines = new BPMNEngineRepository().getByName("ALL").stream().collect(Collectors.toList());
+        final List<EngineLifecycle> engines = new LinkedList<>();
+        engines.addAll(bpelEngines);
+        engines.addAll(bpmnEngines);
+
+        int buttons = 6;
+        int columns = 1 + buttons;
+        int rows = engines.size() + 2; // one empty, one for all
+        panel.setLayout(new GridLayout(rows, columns, 0, 10));
+
+        for (EngineLifecycle engine : engines) {
+            panel.add(new JLabel(engine.toString()));
+
+            panel.add(createButton("install", engine, EngineLifecycle::install));
+            panel.add(createButton("uninstall", engine, EngineLifecycle::uninstall));
+            panel.add(createButton("isInstalled?", engine, (e) -> toast(engine.toString() + " is " + (engine.isInstalled() ? "installed" : "uninstalled"))));
+
+            panel.add(createButton("start", engine, EngineLifecycle::startup));
+            panel.add(createButton("stop", engine, EngineLifecycle::shutdown));
+            panel.add(createButton("isRunning?", engine, (e) -> toast(engine.toString() + " is " + (engine.isRunning() ? "started" : "shutdown"))));
+
         }
 
-        this.setLayout(new GridLayout(engines.size() + 1, 4, 0, 10));
-        this.setSize(400, 300);
-        this.setTitle("Engine Control Center");
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addEmptyRow(panel, columns);
+
+        panel.add(new JLabel("ALL"));
+        panel.add(createAllButton(engines, "install", EngineLifecycle::install));
+        panel.add(createAllButton(engines, "uninstall", EngineLifecycle::uninstall));
+        panel.add(new JLabel());
+        panel.add(createAllButton(engines, "startup", EngineLifecycle::startup));
+        panel.add(createAllButton(engines, "shutdown", EngineLifecycle::shutdown));
+        panel.add(new JLabel());
+
+        return panel;
+    }
+
+    private JButton createAllButton(List<EngineLifecycle> engines, String name, Consumer<EngineLifecycle> f) {
+        JButton button = new JButton(name);
+        button.addActionListener(e -> {
+            for (final EngineLifecycle engine : engines) {
+                executeEngineAction(name, engine, (x) -> f.accept(engine));
+            }
+        });
+        return button;
+    }
+
+    private JButton createButton(final String name, final EngineLifecycle engine, Consumer<EngineLifecycle> action) {
+        JButton button = new JButton(name);
+        button.addActionListener((e) -> executeEngineAction(name, engine, action));
+        return button;
+    }
+
+    private void executeEngineAction(String name, EngineLifecycle engine, Consumer<EngineLifecycle> action) {
+        executeAction(name + " of " + engine.toString(), () -> action.accept(engine));
+    }
+
+    private void executeAction(final String name, final Runnable action) {
+        new Thread() {
+            public void run() {
+                toast("EXECUTE Action " + name);
+                action.run();
+                toast("EXECUTE Action " + name + " COMPLETED");
+            }
+        }.start();
+    }
 
 
-        for (EngineAPI tmpEngine : engines) {
-            final EngineAPI engine = tmpEngine;
+    private void toast(String message) {
+        String toastMessage = "[" + new Date() + "] " + message;
+        System.out.println(toastMessage);
+        actions.insertElementAt(toastMessage, 0);
+        this.validate();
+        this.repaint();
+    }
 
-            add(new JLabel(engine.getName()));
-
-            JButton startButton = new JButton("install");
-            startButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    new Thread() {
-                        public void run() {
-                            engine.install();
-                            System.out.println("Installation of " + engine.getName() + " is complete");
-                        }
-
-                    }.start();
-                }
-
-            });
-            add(startButton);
-
-            startButton = new JButton("startup");
-            startButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    new Thread() {
-                        public void run() {
-                            engine.startup();
-                        }
-
-                    }.start();
-                }
-
-            });
-            add(startButton);
-
-            startButton = new JButton("shutdown");
-            startButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    new Thread() {
-                        public void run() {
-                            engine.shutdown();
-                        }
-
-                    }.start();
-                }
-
-            });
-            add(startButton);
+    private void addEmptyRow(JPanel panel, int columns) {
+        for (int i = 0; i < columns; i++) {
+            panel.add(new JLabel());
         }
-
-
-        add(new JLabel("ALL"));
-
-        JButton startButton = new JButton("install");
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (final EngineAPI engine : engines) {
-                    new Thread() {
-                        public void run() {
-                            engine.install();
-                            System.out.println("Installation of " + engine.getName() + " is complete");
-                        }
-
-                    }.start();
-                }
-
-            }
-
-        });
-        this.add(startButton);
-
-        startButton = new JButton("startup");
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (final EngineAPI engine : engines) {
-                    new Thread() {
-                        public void run() {
-                            engine.startup();
-                        }
-
-                    }.start();
-                }
-
-            }
-
-        });
-        this.add(startButton);
-
-        startButton = new JButton("shutdown");
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (final EngineAPI engine : engines) {
-                    new Thread() {
-                        public void run() {
-                            engine.shutdown();
-                        }
-
-                    }.start();
-                }
-
-            }
-
-        });
-        this.add(startButton);
     }
 }
