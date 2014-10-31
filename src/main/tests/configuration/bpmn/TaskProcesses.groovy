03fb415d3a94da964d69ee590c948ca03ecdd955
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-10-31 10:10:57 +0100

    Improve naming of task processes

--- src/main/tests/configuration/bpmn/TaskProcesses.groovy
+++ src/main/tests/configuration/bpmn/TaskProcesses.groovy
@@ -14,7 +14,7 @@ class TaskProcesses {
     )
 
     public static final BPMNProcess SEQUENCE_FLOW_CONDITIONAL = builder.buildTaskProcess(
-            "SequenceFlowConditional", "A process with three scriptTasks connected by sequenceFlows. " +
+            "SequenceFlow_Conditional", "A process with three scriptTasks connected by sequenceFlows. " +
             "The first scriptTask points to the other tasks with sequenceFlows. " +
             "One of these sequenceFlows is associated with a conditionExpression",
             [
@@ -24,7 +24,7 @@ class TaskProcesses {
     )
 
     public static final BPMNProcess SEQUENCE_FLOW_CONDITIONAL_DEFAULT = builder.buildTaskProcess(
-            "SequenceFlowConditionalDefault", "A process with three scriptTasks connected by sequenceFlows. " +
+            "SequenceFlow_ConditionalDefault", "A process with three scriptTasks connected by sequenceFlows. " +
             "The first scriptTask points to the other tasks with sequenceFlows. " +
             "One of these sequenceFlows is associated with a conditionExpression, the other one is marked as default",
             [
@@ -34,7 +34,7 @@ class TaskProcesses {
     )
 
     public static final BPMNProcess SEQUENCE_FLOW_CONDITIONAL_DEFAULT_NORMAL = builder.buildTaskProcess(
-            "SequenceFlowConditionalDefaultNormal", "A process with four scriptTasks connected by sequenceFlows. " +
+            "SequenceFlow_ConditionalDefault_Normal", "A process with four scriptTasks connected by sequenceFlows. " +
             "The first scriptTask points to the other three tasks with sequenceFlows. " +
             "The first of these sequenceFlows is associated with a conditionExpression, the second one is marked as default and the third has no condition associated. " +
             "This is a special case document in Sec. 13.2.1, p. 427.",
@@ -44,60 +44,60 @@ class TaskProcesses {
             ]
     )
 
-    public static final BPMNProcess MULTI_INSTANCE_SEQUENTIAL = builder.buildTaskProcess(
-            "MultiInstanceSequentialTask", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times",
+    public static final BPMNProcess MULTI_INSTANCE_TASK_SEQUENTIAL = builder.buildTaskProcess(
+            "MultiInstanceTask_Sequential", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times",
             [
                     new BPMNTestCase().assertTask1().assertTask1().assertTask1().assertTask2()
             ]
     )
 
-    public static final BPMNProcess MULTI_INSTANCE_NONE_BEHAVIOR = builder.buildTaskProcess(
-            "MultiInstanceTaskNoneBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to none." +
+    public static final BPMNProcess MULTI_INSTANCE_TASK_NONE_BEHAVIOR = builder.buildTaskProcess(
+            "MultiInstanceTask_NoneBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to none." +
             "The task has a signal boundary event attached that points to another script task. The event should be thrown for every task execution",
             [
                     new BPMNTestCase().assertTask1().assertTask3().assertTask1().assertTask3().assertTask1().assertTask3().assertTask2()
             ]
     )
 
-    public static final BPMNProcess MULTI_INSTANCE_ONE_BEHAVIOR = builder.buildTaskProcess(
-            "MultiInstanceTaskOneBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to one." +
+    public static final BPMNProcess MULTI_INSTANCE_TASK_ONE_BEHAVIOR = builder.buildTaskProcess(
+            "MultiInstanceTask_OneBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to one." +
             "The task has a signal boundary event attached that points to another script task. The event should be thrown once.",
             [
                     new BPMNTestCase().assertTask1().assertTask3().assertTask1().assertTask1().assertTask2()
             ]
     )
 
-    public static final BPMNProcess MULTI_INSTANCE_ALL_BEHAVIOR = builder.buildTaskProcess(
-            "MultiInstanceTaskAllBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to all." +
+    public static final BPMNProcess MULTI_INSTANCE_TASK_ALL_BEHAVIOR = builder.buildTaskProcess(
+            "MultiInstanceTask_AllBehavior", "A scriptTask that is marked as a sequential multiInstance task and is enabled three times and its behavior set to all." +
             "The task has a signal boundary event attached that points to another script task. The event should never be thrown.",
             [
                     new BPMNTestCase().assertTask1().assertTask1().assertTask1().assertTask2()
             ]
     )
 
-    public static final BPMNProcess MULTI_INSTANCE_PARALLEL = builder.buildTaskProcess(
-            "MultiInstanceParallelTask", "A scriptTask that is marked as a parallel multiInstance task and is enabled three times",
+    public static final BPMNProcess MULTI_INSTANCE_TASK_PARALLEL = builder.buildTaskProcess(
+            "MultiInstanceTask_Parallel", "A scriptTask that is marked as a parallel multiInstance task and is enabled three times",
             [
                     new BPMNTestCase().assertTask1().assertTask1().assertTask1().assertTask2()
             ]
     )
 
-    public static final BPMNProcess LOOP_WITH_LOOP_MAXIMUM = builder.buildTaskProcess(
-            "LoopTaskWithLoopMaximum", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to true. Additionally a loopMaximum is set to three.",
+    public static final BPMNProcess LOOP_TASK_LOOP_MAXIMUM = builder.buildTaskProcess(
+            "LoopTask_LoopMaximum", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to true. Additionally a loopMaximum is set to three.",
             [
                     new BPMNTestCase().assertTask1().assertTask1().assertTask1().assertTask2()
             ]
     )
 
     public static final BPMNProcess LOOP_NO_ITERATION_TEST_BEFORE_FALSE = builder.buildTaskProcess(
-            "LoopTaskNoIterationTestBeforeFalse", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to false, but has testBefore set to false and, hence, should be executed once.",
+            "LoopTask_NoIteration_TestBeforeFalse", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to false, but has testBefore set to false and, hence, should be executed once.",
             [
                     new BPMNTestCase().assertTask1().assertTask2()
             ]
     )
 
     public static final BPMNProcess LOOP_NO_ITERATION_TEST_BEFORE_TRUE = builder.buildTaskProcess(
-            "LoopTaskNoIterationTestBeforeTrue", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to false and has testBefore set to true. Hence, the task should never be executed.",
+            "LoopTask_NoIteration_TestBeforeTrue", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to false and has testBefore set to true. Hence, the task should never be executed.",
             [
                     new BPMNTestCase().assertTask2()
             ]
@@ -109,13 +109,13 @@ class TaskProcesses {
             SEQUENCE_FLOW_CONDITIONAL_DEFAULT,
             SEQUENCE_FLOW_CONDITIONAL_DEFAULT_NORMAL,
 
-            MULTI_INSTANCE_SEQUENTIAL,
-            MULTI_INSTANCE_NONE_BEHAVIOR,
-            MULTI_INSTANCE_ONE_BEHAVIOR,
-            MULTI_INSTANCE_ALL_BEHAVIOR,
-            MULTI_INSTANCE_PARALLEL,
+            MULTI_INSTANCE_TASK_SEQUENTIAL,
+            MULTI_INSTANCE_TASK_NONE_BEHAVIOR,
+            MULTI_INSTANCE_TASK_ONE_BEHAVIOR,
+            MULTI_INSTANCE_TASK_ALL_BEHAVIOR,
+            MULTI_INSTANCE_TASK_PARALLEL,
 
-            LOOP_WITH_LOOP_MAXIMUM,
+            LOOP_TASK_LOOP_MAXIMUM,
             LOOP_NO_ITERATION_TEST_BEFORE_FALSE,
             LOOP_NO_ITERATION_TEST_BEFORE_TRUE
     ].flatten() as List<BPMNProcess>
