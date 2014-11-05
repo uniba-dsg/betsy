990c622be8476ab24ca2d03586740aefdfcf04b2
Simon Harrer <simon.harrer@gmail.com>
2014-11-05 10:04:02 +0100

    Revert "Removes statistics."
    This reverts commit eaa9b950ce5ee47cf806a7ebb7f70c9588fea8a2.

---
+++ src/main/groovy/betsy/common/analytics/additional/CsvReportToTestsPerEngineAndGroup.groovy
@@ -0,0 +1,51 @@
+package betsy.common.analytics.additional
+
+import betsy.common.analytics.CsvReportLoader
+import betsy.common.analytics.model.CsvReport
+import betsy.common.analytics.model.Result
+
+import java.nio.file.Paths
+
+
+class CsvReportToTestsPerEngineAndGroup {
+
+    CsvReport report
+
+    void toCsvReport(PrintStream writer) {
+        report.getEngines().each { engine ->
+            int totalSuccessful = 0
+            int totalPartial = 0
+            int totalFailed = 0
+            report.getGroups().each { group ->
+                Collection<Result> results = group.getResultsPerEngine(engine)
+                int successful = results.count { it.partial == Result.Support.TOTAL}
+                totalSuccessful += successful
+                int partial = results.count { it.partial == Result.Support.PARTIAL}
+                totalPartial += partial
+                int failed = results.count { it.partial == Result.Support.NONE}
+                totalFailed += failed
+
+                writer.println "$successful\t${partial == 0 ? '' : partial}\t$failed"
+            }
+
+            writer.println "$totalSuccessful\t${totalPartial == 0 ? '' : totalPartial}\t$totalFailed"
+        }
+
+        writer.println "Engine in %"
+        int total = report.tests.size()
+        report.getEngines().each { engine ->
+            int totalSuccessful = 0
+            report.getGroups().each { group ->
+                Collection<Result> results = group.getResultsPerEngine(engine)
+                totalSuccessful += results.count { it.partial == Result.Support.TOTAL}
+            }
+
+            def successfulInPercent = (int) Math.round(((double) totalSuccessful / total * 100))
+            writer.println "${engine.name}\t$totalSuccessful\t${successfulInPercent}%\t${100-successfulInPercent}%"
+        }
+    }
+
+    public static void main(String[] args) {
+        new CsvReportToTestsPerEngineAndGroup(report: new CsvReportLoader(Paths.get(args[0]), new CsvReport()).load()).toCsvReport(System.out)
+    }
+}
