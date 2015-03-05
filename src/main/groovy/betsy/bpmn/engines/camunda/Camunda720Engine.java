82495cea18a9b5f6767c2de865955bcf4c59f51f
Lea-Louisa Maaß <lea-louisa-katharina.maass@stud.uni-bamberg.de>
2015-03-05 12:47:27 +0100

    add class Camunda720Engine for the new CamundaBPM Version 7.2.0

---
+++ src/main/groovy/betsy/bpmn/engines/camunda/Camunda720Engine.java
@@ -0,0 +1,45 @@
+package betsy.bpmn.engines.camunda;
+
+import betsy.common.tasks.ConsoleTasks;
+import betsy.common.tasks.WaitTasks;
+import betsy.common.util.ClasspathHelper;
+
+import java.nio.file.Path;
+
+/**
+ * Created by stlmaass on 05.03.2015.
+ */
+public class Camunda720Engine extends CamundaEngine {
+
+    @Override
+    public String getName() {
+        return "camunda720";
+    }
+
+    @Override
+    public Path getXsltPath() {
+        return ClasspathHelper.getFilesystemPathFromClasspathPath("/bpmn/" + super.getName());
+    }
+
+    @Override
+    public String getTomcatName() {
+        return "apache-tomcat-7.0.50";
+    }
+
+    @Override
+    public void install() {
+        CamundaInstaller camundaInstaller = new CamundaInstaller();
+        camundaInstaller.setDestinationDir(getServerPath());
+        camundaInstaller.setFileName("camunda-bpm-tomcat-7.2.0.zip");
+        camundaInstaller.setTomcatName(getTomcatName());
+        camundaInstaller.install();
+    }
+
+    @Override
+    public void startup() {
+        ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath(), "camunda_startup.bat"));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getServerPath().resolve("camunda_startup.sh")));
+        WaitTasks.waitForAvailabilityOfUrl(30_000, 500, getCamundaUrl());
+    }
+
+}
