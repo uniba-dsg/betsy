417569777b16cb854cbdd56f4e0a1ff3c4b7c54a
Christian Kremitzl <christian-dominik.kremitzl@stud.uni-bamberg.de>
2015-03-11 11:25:21 +0100

    Add and integrate process and test case for testing true parallelism

--- src/main/groovy/betsy/bpmn/validation/ProcessValidator.java
+++ src/main/groovy/betsy/bpmn/validation/ProcessValidator.java
@@ -124,6 +124,9 @@ public class ProcessValidator {
         List<String> allowedLogMessages = new LinkedList<>();
         allowedLogMessages.addAll(allowedScriptTasks);
         allowedLogMessages.add("CREATE_LOG_FILE");
+        allowedLogMessages.add("CREATE_TIMESTAMP_LOG_1");
+        allowedLogMessages.add("CREATE_TIMESTAMP_LOG_2");
+        allowedLogMessages.add("EVALUATE_TIMESTAMP_LOGS");
         return allowedLogMessages.toArray(new String[allowedLogMessages.size()]);
     }
 
