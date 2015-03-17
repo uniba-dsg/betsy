683154b98f26ea423122aa7b432692653119f30e
Christian Kremitzl <christian-dominik.kremitzl@stud.uni-bamberg.de>
2015-03-17 11:06:52 +0100

    Renaming method.

--- src/test/groovy/betsy/bpmn/engines/OverlappingTimestampCheckerTests.java
+++ src/test/groovy/betsy/bpmn/engines/OverlappingTimestampCheckerTests.java
@@ -2,16 +2,13 @@ package betsy.bpmn.engines;
 
 import betsy.bpmn.model.BPMNAssertions;
 import betsy.common.tasks.FileTasks;
-import groovy.ui.SystemOutputInterceptor;
 import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
@@ -27,7 +24,7 @@ public class OverlappingTimestampCheckerTests {
 
     private static final String[] TIMESTAMPS = {"1420110000000", "1420111800000", "1420113600000", "1420115400000"};
 
-    public void createOverlappingLogFilesWithTimestamps(int first, int second, int third, int fourth) throws IOException {
+    public void createLogFilesWithTimestamps(int first, int second, int third, int fourth) throws IOException {
         String contentLogOne = String.format("%s%n%s", TIMESTAMPS[first], TIMESTAMPS[second]);
         String contentLogTwo = String.format("%s%n%s", TIMESTAMPS[third], TIMESTAMPS[fourth]);
 
@@ -40,7 +37,7 @@ public class OverlappingTimestampCheckerTests {
 
     @Test
     public void testCheckParallelismWithOneOverlapsTwo() throws Exception {
-        createOverlappingLogFilesWithTimestamps(0, 2, 1, 3);
+        createLogFilesWithTimestamps(0, 2, 1, 3);
         OverlappingTimestampChecker otc = new OverlappingTimestampChecker(PATH_LOG_FILE, PATH_LOG_ONE, PATH_LOG_TWO);
         otc.checkParallelism();
         assertTrue(executionWasParallel());
@@ -48,7 +45,7 @@ public class OverlappingTimestampCheckerTests {
 
     @Test
     public void testCheckParallelismWithOneBeforeTwo() throws Exception {
-        createOverlappingLogFilesWithTimestamps(0, 1, 2, 3);
+        createLogFilesWithTimestamps(0, 1, 2, 3);
         OverlappingTimestampChecker otc = new OverlappingTimestampChecker(PATH_LOG_FILE, PATH_LOG_ONE, PATH_LOG_TWO);
         otc.checkParallelism();
         assertFalse(executionWasParallel());
@@ -56,7 +53,7 @@ public class OverlappingTimestampCheckerTests {
 
     @Test
     public void testCheckParallelismWithOneEqualsTwo() throws Exception {
-        createOverlappingLogFilesWithTimestamps(0, 1, 0, 1);
+        createLogFilesWithTimestamps(0, 1, 0, 1);
         OverlappingTimestampChecker otc = new OverlappingTimestampChecker(PATH_LOG_FILE, PATH_LOG_ONE, PATH_LOG_TWO);
         otc.checkParallelism();
         assertTrue(executionWasParallel());
@@ -64,7 +61,7 @@ public class OverlappingTimestampCheckerTests {
 
     @Test
     public void testCheckParallelismWithOneDuringTwo() throws Exception {
-        createOverlappingLogFilesWithTimestamps(1, 2, 0, 3);
+        createLogFilesWithTimestamps(1, 2, 0, 3);
         OverlappingTimestampChecker otc = new OverlappingTimestampChecker(PATH_LOG_FILE, PATH_LOG_ONE, PATH_LOG_TWO);
         otc.checkParallelism();
         assertTrue(executionWasParallel());
@@ -72,7 +69,7 @@ public class OverlappingTimestampCheckerTests {
 
     @Test
     public void testCheckParallelismWithOneAfterTwo() throws Exception {
-        createOverlappingLogFilesWithTimestamps(2, 3, 0, 1);
+        createLogFilesWithTimestamps(2, 3, 0, 1);
         OverlappingTimestampChecker otc = new OverlappingTimestampChecker(PATH_LOG_FILE, PATH_LOG_ONE, PATH_LOG_TWO);
         otc.checkParallelism();
         assertFalse(executionWasParallel());
