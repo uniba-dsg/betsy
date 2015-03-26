cf8e3cc01643a42d510c863f166854822cfb3e20
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-03-26 09:48:24 +0100

    Corrected install dir for jbpm620

--- src/main/groovy/betsy/bpmn/engines/jbpm/JbpmEngine620.java
+++ src/main/groovy/betsy/bpmn/engines/jbpm/JbpmEngine620.java
@@ -32,7 +32,7 @@ public class JbpmEngine620 extends JbpmEngine {
     @Override
     public void install() {
         JbpmInstaller jbpmInstaller = new JbpmInstaller();
-        jbpmInstaller.setDestinationDir(getServerPath());
+        jbpmInstaller.setDestinationDir(getServerPath().getParent());
         jbpmInstaller.setFileName("jbpm-6.2.0.Final-installer-full.zip");
         jbpmInstaller.install();
     }
