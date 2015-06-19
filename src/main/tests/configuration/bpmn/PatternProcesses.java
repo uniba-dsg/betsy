141c3df8fc9a050d352df5aef8685329b87b566d
Frederik Müller <frederik-christian.mueller@stud.uni-bamberg.de>
2015-06-19 16:27:55 +0200

    add Test for WCP05

--- src/main/tests/configuration/bpmn/PatternProcesses.java
+++ src/main/tests/configuration/bpmn/PatternProcesses.java
@@ -20,10 +20,15 @@ public class PatternProcesses {
     public static final BPMNProcess SYNCHRONIZATION_PATTERN = BPMNProcessBuilder.buildPatternProcess("WCP03Synchronization", "A Process for Synchronising 3 branches into a single branch, Task4 should be only executed once if the synchronization is working correctly",
             new BPMNTestCase().assertTask1().assertTask2().assertTask3().assertTask4());
 
+    public static final BPMNProcess MERGE_PATTERN = BPMNProcessBuilder.buildPatternProcess("WCP05Multimerge",
+            "A Process for Merging 3 branches into a single branch without using a converging gateway, task 4 should be executed 3 times (a new instance is created with each incoming flow",
+            new BPMNTestCase().assertTask1().assertTask2().assertTask3().assertTask4().assertTask4().assertTask4());
+
     public static final List<BPMNProcess> PATTERNS = Arrays.asList(
             SEQUENCE_PATTERN,
 			PARALLEL_PATTERN,
-            SYNCHRONIZATION_PATTERN
+            SYNCHRONIZATION_PATTERN,
+            MERGE_PATTERN
 
     );
 
