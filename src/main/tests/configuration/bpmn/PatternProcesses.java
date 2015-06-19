b6c0a7d7f77b4df8391e45912d85ba213eee27bb
Severin Sobetzko <severin.sobetzko@uni-bamberg.de>
2015-06-19 16:26:18 +0200

    Added WCP04TestCase and modified the corresponding BPMN file

--- src/main/tests/configuration/bpmn/PatternProcesses.java
+++ src/main/tests/configuration/bpmn/PatternProcesses.java
@@ -20,10 +20,17 @@ public class PatternProcesses {
     public static final BPMNProcess SYNCHRONIZATION_PATTERN = BPMNProcessBuilder.buildPatternProcess("WCP03Synchronization", "A Process for Synchronising 3 branches into a single branch, Task4 should be only executed once if the synchronization is working correctly",
             new BPMNTestCase().assertTask1().assertTask2().assertTask3().assertTask4());
 
+    public static final BPMNProcess EXCLUSIVE_PATTERN = BPMNProcessBuilder.buildPatternProcess("WCP04ExclusiveChoice","A Process for a decision based workflow, if the process contains a it should execute Task1, if it contains b it should execute Task2, if neither of these is executed, the Default Task should be executed",
+            new BPMNTestCase().inputA().assertTask1(),
+            new BPMNTestCase().inputB().assertTask2(),
+            new BPMNTestCase().inputC().assertTask3()
+    );
+
     public static final List<BPMNProcess> PATTERNS = Arrays.asList(
             SEQUENCE_PATTERN,
 			PARALLEL_PATTERN,
-            SYNCHRONIZATION_PATTERN
+            SYNCHRONIZATION_PATTERN,
+            EXCLUSIVE_PATTERN
 
     );
 
