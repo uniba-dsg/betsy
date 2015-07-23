bef2444b6b5c1ab31fb3593e3dba4719c43d2a3f
Simon Harrer <simon.harrer@gmail.com>
2015-07-23 11:57:16 +0200

    Fixes problem by running the BPMN test first that does not use soapUI and running BPEL test second that does soapUI.

--- src/test/groovy/betsy/SystemTest.java
+++ src/test/groovy/betsy/SystemTest.java
@@ -1,7 +1,9 @@
 package betsy;
 
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,22 +11,21 @@ import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SystemTest {
 
     @Test
-    public void testBpmnActivitiSequenceFlow() throws IOException {
+    public void test_A_BpmnActivitiSequenceFlow() throws IOException {
         Main.main("bpmn", "activiti", "SequenceFlow");
 
         assertEquals("[SequenceFlow;activiti;basics;1;0;1;1]", Files.readAllLines(Paths.get("test/reports/results.csv")).toString());
     }
 
-    @Test @Ignore(value = "soapUI does not clean up its file resources, causing 'test/' folder to be undeletable on windows")
-    public void testBpelOdeSequence() throws IOException, InterruptedException {
+    @Test
+    public void test_B_BpelOdeSequence() throws IOException, InterruptedException {
         Main.main("bpel", "ode", "sequence");
 
         assertEquals("[Sequence;ode;structured;1;0;1;1]", Files.readAllLines(Paths.get("test/reports/results.csv")).toString());
-
-        Thread.sleep(15_000); // to release any file locks that may be hold, but this does not help!
     }
 
 }
