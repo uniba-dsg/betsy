10a11c71f57c3205ef0d6f6f51998d1e94b278a8
Simon Harrer <simon.harrer@gmail.com>
2015-09-11 14:26:31 +0200

    Use bin folder as base path for executing the wso2 startup/shutdown scripts

--- src/main/groovy/betsy/bpel/engines/wso2/Wso2Engine_v3_1_0.java
+++ src/main/groovy/betsy/bpel/engines/wso2/Wso2Engine_v3_1_0.java
@@ -50,7 +50,7 @@ public class Wso2Engine_v3_1_0 extends AbstractLocalBPELEngine {
     public void startup() {
         ConsoleTasks.executeOnWindows(ConsoleTasks.CliCommand.build(getServerPath(), "startup.bat"));
 
-        ConsoleTasks.executeOnUnix(ConsoleTasks.CliCommand.build(getBinDir().resolve("wso2server.sh")).values("start")); // start wso2 in background
+        ConsoleTasks.executeOnUnix(ConsoleTasks.CliCommand.build(getBinDir(), getBinDir().resolve("wso2server.sh")).values("start")); // start wso2 in background
         WaitTasks.sleep(2000);
         WaitTasks.waitForSubstringInFile(120_000, 500, getLogsFolder().resolve("wso2carbon.log"), "WSO2 Carbon started in ");
     }
@@ -71,7 +71,7 @@ public class Wso2Engine_v3_1_0 extends AbstractLocalBPELEngine {
     public void shutdown() {
         ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build("taskkill").values("/FI", "WINDOWTITLE eq " + getName()));
 
-        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getBinDir().resolve("wso2server.sh")).values("stop"));
+        ConsoleTasks.executeOnUnixAndIgnoreError(ConsoleTasks.CliCommand.build(getBinDir(), getBinDir().resolve("wso2server.sh")).values("stop"));
     }
 
     @Override
