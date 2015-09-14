4b376fb6ac3d1ca2c98ee04faeb3c18cc3700a53
Simon Harrer <simon.harrer@gmail.com>
2015-09-14 14:13:56 +0200

    Update OpenESB 2.3 and 2.3.1 as well according to the changes for Openesb 2.2

--- src/main/groovy/betsy/bpel/engines/openesb/OpenEsb231Engine.java
+++ src/main/groovy/betsy/bpel/engines/openesb/OpenEsb231Engine.java
@@ -1,6 +1,7 @@
 package betsy.bpel.engines.openesb;
 
 import betsy.common.util.ClasspathHelper;
+import betsy.common.util.OperatingSystem;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,8 +18,15 @@ public class OpenEsb231Engine extends OpenEsbEngine {
 
     @Override
     public void install() {
-        new OpenEsbInstaller(Paths.get("server/openesb231"), "openesb-v231-installer-windows.exe",
-                ClasspathHelper.getFilesystemPathFromClasspathPath("/bpel/openesb231/state.xml.template")).install();
+        if(OperatingSystem.WINDOWS) {
+            new OpenEsbInstaller(getServerPath(),
+                    "openesb-v231-installer-windows.exe",
+                    ClasspathHelper.getFilesystemPathFromClasspathPath("/bpel/openesb231/state.xml.template")).install();
+        } else {
+            new OpenEsbInstaller(getServerPath(),
+                    "openesb-v231-installer-linux.sh",
+                    ClasspathHelper.getFilesystemPathFromClasspathPath("/bpel/openesb231/state.xml.template")).install();
+        }
     }
 
 }
