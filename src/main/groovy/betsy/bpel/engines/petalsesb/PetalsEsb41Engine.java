9bd1408d573317125e68247b9e8d55252b8b4f05
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-09-16 14:01:48 +0200

    Enable cli operations on petals 4.1

--- src/main/groovy/betsy/bpel/engines/petalsesb/PetalsEsb41Engine.java
+++ src/main/groovy/betsy/bpel/engines/petalsesb/PetalsEsb41Engine.java
@@ -1,11 +1,16 @@
 package betsy.bpel.engines.petalsesb;
 
+import betsy.common.tasks.ConsoleTasks;
 import betsy.common.util.ClasspathHelper;
+import org.apache.log4j.Logger;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PetalsEsb41Engine extends PetalsEsbEngine {
+
+    private static final Logger LOGGER = Logger.getLogger(PetalsEsb41Engine.class);
+
     @Override
     public String getName() {
         return "petalsesb41";
@@ -20,6 +25,18 @@ public class PetalsEsb41Engine extends PetalsEsbEngine {
     }
 
     @Override
+    public Path getPetalsCliBinFolder() {return getServerPath().resolve("petals-cli-2.0.0/bin");}
+
+    @Override
+    public void shutdown() {
+        try {
+            ConsoleTasks.executeOnWindowsAndIgnoreError(ConsoleTasks.CliCommand.build(getPath(), "taskkill").values("/FI", "WINDOWTITLE eq OW2*"));
+        } catch (Exception ignore) {
+            LOGGER.info("COULD NOT STOP ENGINE " + getName());
+        }
+    }
+
+    @Override
     public void install() {
         PetalsEsbInstaller installer = new PetalsEsbInstaller(this);
         installer.setServerDir(Paths.get("server/petalsesb41"));
@@ -28,6 +45,7 @@ public class PetalsEsb41Engine extends PetalsEsbEngine {
         installer.setBpelComponentPath(Paths.get("server/petalsesb41/petals-esb-distrib-4.1.0/esb-components/petals-se-bpel-1.1.0.zip"));
         installer.setSoapComponentPath(Paths.get("server/petalsesb41/petals-esb-distrib-4.1.0/esb-components/petals-bc-soap-4.2.0.zip"));
         installer.setSourceFile(Paths.get("server/petalsesb41/petals-esb-distrib-4.1.0/esb/petals-esb-4.1.zip"));
+        installer.setCliFile(Paths.get("server/petalsesb41/petals-esb-distrib-4.1.0/esb/petals-cli-2.0.0.zip"));
         installer.install();
     }
 
