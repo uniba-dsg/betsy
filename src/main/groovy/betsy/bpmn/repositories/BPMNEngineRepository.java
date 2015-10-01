f333696387ba64accb5dd091dad829a764ae5c8d
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-10-01 16:13:53 +0200

    Add activiti 5.18.0

--- src/main/groovy/betsy/bpmn/repositories/BPMNEngineRepository.java
+++ src/main/groovy/betsy/bpmn/repositories/BPMNEngineRepository.java
@@ -2,6 +2,7 @@ package betsy.bpmn.repositories;
 
 import betsy.bpmn.engines.AbstractBPMNEngine;
 import betsy.bpmn.engines.activiti.Activiti5170Engine;
+import betsy.bpmn.engines.activiti.Activiti5180Engine;
 import betsy.bpmn.engines.activiti.ActivitiEngine;
 import betsy.bpmn.engines.camunda.Camunda710Engine;
 import betsy.bpmn.engines.camunda.Camunda720Engine;
@@ -10,6 +11,7 @@ import betsy.bpmn.engines.camunda.CamundaEngine;
 import betsy.bpmn.engines.jbpm.JbpmEngine;
 import betsy.bpmn.engines.jbpm.JbpmEngine610;
 import betsy.bpmn.engines.jbpm.JbpmEngine620;
+import betsy.bpmn.engines.jbpm.JbpmEngine630;
 import betsy.common.repositories.Repository;
 
 import java.util.ArrayList;
@@ -19,8 +21,9 @@ import java.util.List;
 
 public class BPMNEngineRepository {
     public BPMNEngineRepository() {
-        List<AbstractBPMNEngine> all = new ArrayList<>(Arrays.asList(new CamundaEngine(), new Camunda710Engine(),
-                new Camunda720Engine(), new Camunda730Engine(), new ActivitiEngine(), new Activiti5170Engine(),
+        List<AbstractBPMNEngine> all = new ArrayList<>(Arrays.asList(
+                new CamundaEngine(), new Camunda710Engine(), new Camunda720Engine(), new Camunda730Engine(),
+                new ActivitiEngine(), new Activiti5170Engine(), new Activiti5180Engine(),
                 new JbpmEngine(), new JbpmEngine610(), new JbpmEngine620()));
         repo.put("ALL", all);
 
