d4af4680fe5353212cfb18c122a7ab8022bee22b
Christoph Bröker <christoph-broeker@web.de>
2015-12-10 16:55:57 +0100

    Extended TimeoutException with more constructors

--- src/main/groovy/timeouts/TimeoutException.java
+++ src/main/groovy/timeouts/TimeoutException.java
@@ -14,29 +14,39 @@ public class TimeoutException extends RuntimeException {
 
     private Optional<Timeout> timeout;
 
-    public TimeoutException() {
-    }
-
     public TimeoutException(Timeout timeout) {
-        this.timeout = Optional.of(timeout);
+        this.timeout = Optional.ofNullable(timeout);
     }
 
-    public TimeoutException(String var1) {
-        super(var1);
-        timeout = Optional.empty();
+    public TimeoutException(String message) {
+        super(message);
+        this.timeout = Optional.empty();
     }
 
-    public TimeoutException(String var1, Throwable var2) {
-        super(var1, var2);
-        timeout = Optional.empty();
+    public TimeoutException(String message, Timeout timeout) {
+        super(message);
+        this.timeout = Optional.ofNullable(timeout);
     }
 
-    public TimeoutException(Throwable var1) {
-        super(var1);
-        timeout = Optional.empty();
+    public TimeoutException(String message, Throwable throwable) {
+        super(message, throwable);
+        this.timeout = Optional.empty();
     }
 
+    public TimeoutException(String message, Throwable throwable, Timeout timeout) {
+        super(message, throwable);
+        this.timeout = Optional.ofNullable(timeout);
+    }
 
+    public TimeoutException(Throwable throwable) {
+        super(throwable);
+        this.timeout = Optional.empty();
+    }
+
+    public TimeoutException(Throwable throwable, Timeout timeout) {
+        super(throwable);
+        this.timeout = Optional.ofNullable(timeout);
+    }
 
     public Optional<Timeout> getTimeout(){
             return timeout;
