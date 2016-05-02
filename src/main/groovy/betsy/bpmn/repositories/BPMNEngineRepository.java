0aa83fc29b6ecee933060e9723c452ce34efab3d
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2016-05-02 22:43:27 +0200

    add activiti 5.20.0

--- src/main/groovy/betsy/bpmn/repositories/BPMNEngineRepository.java
+++ src/main/groovy/betsy/bpmn/repositories/BPMNEngineRepository.java
@@ -18,7 +18,7 @@ public class BPMNEngineRepository {
     public BPMNEngineRepository() {
         List<AbstractBPMNEngine> all = new ArrayList<>(Arrays.asList(
                 new CamundaEngine(), new Camunda710Engine(), new Camunda720Engine(), new Camunda730Engine(), new Camunda740Engine(),
-                new Activiti5151Engine(), new ActivitiEngine(), new Activiti5170Engine(), new Activiti5180Engine(), new Activiti5190Engine(), new Activiti600Beta1Engine(),
+                new Activiti5151Engine(), new ActivitiEngine(), new Activiti5170Engine(), new Activiti5180Engine(), new Activiti5190Engine(), new Activiti5200Engine(), new Activiti600Beta1Engine(),
                 new JbpmEngine(), new JbpmEngine610(), new JbpmEngine620(), new JbpmEngine630()));
         repo.put("ALL", all);
 
