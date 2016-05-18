c4b6d34c5578796eb83c0345bd7b5a2b1f15809f
Christoph Bröker <christoph-broeker@web.de>
2016-05-18 12:49:43 +0200

    changed batch file for windows

--- docker/docker_create_constraints_cmd.bat
+++ docker/docker_create_constraints_cmd.bat
@@ -1,3 +1,3 @@
 call environment.bat %1
 shift
-docker create --name %1 --cpu-shares %2 --device-read-bps=/dev/sda:%3mb  --device-write-bps=/dev/sda:%3mb --memory=%4mb %5 sh betsy %6 %7 %8 %9
+docker create --name %1 --device-read-bps=/dev/sda:%2mb  --device-write-bps=/dev/sda:%2mb --memory=%3mb %4 sh betsy %5 %6 %7 %8
