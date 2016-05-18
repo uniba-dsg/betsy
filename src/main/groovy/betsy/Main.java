bf5fe2e6c73a67b545a46de3ef27cc2e8b6f804f
Christoph Bröker <christoph-broeker@web.de>
2016-05-18 12:40:16 +0200

    Added docker to main

--- src/main/groovy/betsy/Main.java
+++ src/main/groovy/betsy/Main.java
@@ -2,6 +2,7 @@ package betsy;
 
 import betsy.bpel.BPELMain;
 import betsy.bpmn.BPMNMain;
+import betsy.common.virtual.DockerMain;
 import betsy.tools.JsonGenerator;
 import betsy.common.timeouts.TimeoutCalibrator;
 
@@ -28,6 +29,8 @@ public class Main {
             JsonGenerator.main(new String[]{});
         } else if ("calibrate".equalsIgnoreCase(args[0])) {
             TimeoutCalibrator.main(createArgsWithoutFirstValue(args));
+        }else if ("docker".equalsIgnoreCase(args[0])) {
+            DockerMain.main(createArgsWithoutFirstValue(args));
         }else {
             printUsage();
         }
@@ -40,6 +43,6 @@ public class Main {
     }
 
     private static void printUsage() {
-        System.out.println("The first argument must be bpel, bpmn, engine, process, analytics, json or calibrate");
+        System.out.println("The first argument must be bpel, bpmn, engine, process, analytics, json, docker or calibrate");
     }
 }
