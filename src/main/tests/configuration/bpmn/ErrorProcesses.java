31364533197e5471353a119b40ff0473ebc55dc3
Matthias Geiger <matthias.geiger@uni-bamberg.de>
2016-05-24 19:45:14 +0200

    add BPMN construct descriptions (#75)
    * add construct descriptions for BasicProcesses
    
    * add construct descriptions for ActivityProcesses
    
    * add construct descriptions for DataProcesses
    
    * add construct descriptions for ErrorProcesses
    
    * add construct descriptions for GatewayProcesses
    
    * add construct descriptions for EventProcesses
    
    * add missing spaces in String concatenations

--- src/main/tests/configuration/bpmn/ErrorProcesses.java
+++ src/main/tests/configuration/bpmn/ErrorProcesses.java
@@ -13,11 +13,31 @@ import java.util.List;
  */
 class ErrorProcesses {
 
+    /*
+    * ERROR CONSTRUCTS
+    */
+    public static final Construct CONSTRUCT_PARALLEL_GATEWAY_COND = new Construct(Groups.ERRORS, "ParallelGateway_Conditions",
+            "Checking the detection of invalid conditions attached to parallel gateways.");
+
+    public static final Construct CONSTRUCT_INVALID_GATEWAY_COMBINATIONS = new Construct(Groups.ERRORS, "InvalidGatewayCombinations",
+            "Checking the detection of invalid combinations of gateway types resulting, e.g., resulting in deadlocks.");
+
+    public static final Construct CONSTRUCT_INVALID_LOOP_COND = new Construct(Groups.ERRORS, "InvalidLoopConditions",
+            "Checking the detection of invalid loop conditions.");
+
+    public static final Construct CONSTRUCT_INVALID_TOKEN_QUANTITY = new Construct(Groups.ERRORS, "InvalidTokenQuantity",
+            "Checking the detection of invalid token quantities.");
+
+
+     /*
+     * FEATURE TESTS
+     */
+
     public static final EngineIndependentProcess PARALLEL_GATEWAY_WITH_CONDITIONS = BPMNProcessBuilder.buildErrorProcess(
             "ParallelGateway_Conditions", "A process with four scriptTasks and two parallelGateways. " +
                     "Two of the scriptTasks are surrounded by the parallelGateways and the sequenceFlows pointing to the mergine gateway have conditions. " +
                     "These conditions should be ignored by an engine.",
-            new Feature(new Construct(Groups.ERRORS, "ParallelGateway_Conditions"), "ParallelGateway_Conditions"),
+            new Feature(CONSTRUCT_PARALLEL_GATEWAY_COND, "ParallelGateway_Conditions"),
             new BPMNTestCase().inputA().assertTask1().assertTask2().assertTask3(),
             new BPMNTestCase().inputB().assertTask1().assertTask2().assertTask3(),
             new BPMNTestCase().inputAB().assertTask1().assertTask2().assertTask3(),
@@ -28,7 +48,7 @@ class ErrorProcesses {
             "ExclusiveDiverging_ParallelConverging", "A process with four scriptTasks, a diverging exclusiveGateway and a converging parallelGateway. " +
             "Two scriptTasks are enclosed by the gateways and the execution should deadlock, because only one incoming branch of the parallelGateway " +
             "should ever be executed. Hence, the scriptTask following the parallelGateway should never be executed.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidGatewayCombinations"), "ExclusiveDiverging_ParallelConverging"),
+            new Feature(CONSTRUCT_INVALID_GATEWAY_COMBINATIONS, "ExclusiveDiverging_ParallelConverging"),
             new BPMNTestCase().inputA().assertTask1(),
             new BPMNTestCase().inputB().assertTask2(),
             new BPMNTestCase().inputAB().assertTask1()
@@ -38,7 +58,7 @@ class ErrorProcesses {
             "InclusiveDiverging_ParallelConverging", "A process with four scriptTasks, a diverging inclusiveGateway and a converging parallelGateway. " +
             "Two scriptTasks are enclosed by the gateways and the execution should deadlock if only one incoming branch of the parallelGateway " +
             "is enabled. Hence, the scriptTask following the parallelGateway should only be executed in a single case.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidGatewayCombinations"), "InclusiveDiverging_ParallelConverging"),
+            new Feature(CONSTRUCT_INVALID_GATEWAY_COMBINATIONS, "InclusiveDiverging_ParallelConverging"),
             new BPMNTestCase().inputA().assertTask1(),
             new BPMNTestCase().inputB().assertTask2(),
             new BPMNTestCase().inputAB().assertTask1().assertTask2().assertTask3()
@@ -46,34 +66,34 @@ class ErrorProcesses {
 
     public static final EngineIndependentProcess LOOP_TASK_NEGATIVE_LOOP_MAXIMUM = BPMNProcessBuilder.buildErrorProcess(
             "LoopTask_NegativeLoopMaximum", "A scriptTask with standardLoopCharacteristics and a condition that always evaluates to true. Additionally a loopMaximum is set to minus one.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidLoopConditions"), "LoopTask_NegativeLoopMaximum"),
+            new Feature(CONSTRUCT_INVALID_LOOP_COND, "LoopTask_NegativeLoopMaximum"),
             new BPMNTestCase().assertGenericError()
     );
 
     public static final EngineIndependentProcess MULTI_INSTANCE_TASK_NEGATIVE_LOOP_CARDINALITY = BPMNProcessBuilder.buildErrorProcess(
             "MultiInstanceTask_NegativeLoopCardinality", "A scriptTask that is marked as a sequential multiInstance task and is enabled minus one times.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidLoopConditions"), "MultiInstanceTask_NegativeLoopCardinality"),
+            new Feature(CONSTRUCT_INVALID_LOOP_COND, "MultiInstanceTask_NegativeLoopCardinality"),
             new BPMNTestCase().assertGenericError()
     );
 
     public static final EngineIndependentProcess TOKEN_START_QUANTITY_TWO = BPMNProcessBuilder.buildErrorProcess(
-            "Token_StartQuantity_Two", "A process with a scriptTask with completionQuantity=1 and, immediately afterwards," +
+            "Token_StartQuantity_Two", "A process with a scriptTask with completionQuantity=1 and, immediately afterwards, " +
                     "a scriptTask with startQuantity=2. Since there will never two tokens arrive, the scriptTask must not be executed.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidTokenQuantity"), "Token_StartQuantity_Two"),
+            new Feature(CONSTRUCT_INVALID_TOKEN_QUANTITY, "Token_StartQuantity_Two"),
             new BPMNTestCase().assertTask1()
     );
 
     public static final EngineIndependentProcess TOKEN_START_QUANTITY_ZERO = BPMNProcessBuilder.buildErrorProcess(
             "Token_StartQuantity_Zero", "A process with a scriptTask with startQuantity=0. " +
                     "Since startQuantity must not be zero, the process must not be executed.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidTokenQuantity"), "Token_StartQuantity_Zero"),
+            new Feature(CONSTRUCT_INVALID_TOKEN_QUANTITY, "Token_StartQuantity_Zero"),
             new BPMNTestCase().assertGenericError()
     );
 
     public static final EngineIndependentProcess TOKEN_COMPLETION_QUANTITY_ZERO = BPMNProcessBuilder.buildErrorProcess(
             "Token_CompletionQuantity_Zero", "A process with a scriptTask with completionQuantity=0. " +
             "Since completionQuantity must not be zero, the process must not be executed.",
-            new Feature(new Construct(Groups.ERRORS, "InvalidTokenQuantity"), "Token_CompletionQuantity_Zero"),
+            new Feature(CONSTRUCT_INVALID_TOKEN_QUANTITY, "Token_CompletionQuantity_Zero"),
             new BPMNTestCase().assertGenericError()
     );
 
