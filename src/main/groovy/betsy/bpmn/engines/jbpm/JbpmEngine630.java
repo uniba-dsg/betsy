fab67a95fd6a772f9fa3056a02e1f118a8a646f1
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2016-09-27 15:55:39 +0200

    change deployment check url

--- src/main/groovy/betsy/bpmn/engines/jbpm/JbpmEngine630.java
+++ src/main/groovy/betsy/bpmn/engines/jbpm/JbpmEngine630.java
@@ -33,7 +33,7 @@ public class JbpmEngine630 extends JbpmEngine {
     @Override
     protected JbpmApiBasedProcessInstanceOutcomeChecker createProcessOutcomeChecker(String name) {
         String url = getJbpmnUrl() + "/rest/history/instance/1";
-        String deployCheckUrl = getJbpmnUrl() + "/rest/deployment/" + getDeploymentId(name) + "/processes";
+        String deployCheckUrl = getJbpmnUrl() + "/rest/deployment/" + getDeploymentId(name);
         return new JbpmApiBasedProcessInstanceOutcomeChecker(url, deployCheckUrl, getDeploymentId(name));
     }
 
